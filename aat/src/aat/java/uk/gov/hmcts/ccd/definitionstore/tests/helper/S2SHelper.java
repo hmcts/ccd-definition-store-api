cfd54baaf247c9f931786fe641c629055d4a2dfb
Tim Jacomb <tim.jacomb@hmcts.net>
2018-05-09 15:50:28 +0100

    Make test pass

--- aat/src/aat/java/uk/gov/hmcts/ccd/definitionstore/tests/helper/S2SHelper.java
+++ aat/src/aat/java/uk/gov/hmcts/ccd/definitionstore/tests/helper/S2SHelper.java
@@ -1,25 +1,35 @@
 package uk.gov.hmcts.ccd.definitionstore.tests.helper;
 
-import feign.Feign;
-import feign.jackson.JacksonDecoder;
-import feign.jackson.JacksonEncoder;
-import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
-import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.String.format;
 
 public class S2SHelper {
 
-    private final ServiceAuthTokenGenerator tokenGenerator;
+    private final String s2sUrl;
+    private final String secret;
+    private final String microservice;
+    private final GoogleAuthenticator googleAuthenticator;
 
     public S2SHelper(final String s2sUrl, final String secret, final String microservice) {
-        final ServiceAuthorisationApi serviceAuthorisationApi = Feign.builder()
-            .encoder(new JacksonEncoder())
-            .decoder(new JacksonDecoder())
-            .target(ServiceAuthorisationApi.class, s2sUrl);
-
-        this.tokenGenerator = new ServiceAuthTokenGenerator(secret, microservice, serviceAuthorisationApi);
+        this.s2sUrl = s2sUrl;
+        this.secret = secret;
+        this.microservice = microservice;
+        this.googleAuthenticator = new GoogleAuthenticator();
     }
 
+    // Weird dependency problems with feign in this project
     public String getToken() {
-        return tokenGenerator.generate();
+        final String oneTimePassword = format("%06d", googleAuthenticator.getTotpPassword(secret));
+
+        Map<String, String> signInDetails = new HashMap<>();
+        signInDetails.put("microservice", this.microservice);
+        signInDetails.put("oneTimePassword", oneTimePassword);
+
+        return new RestTemplate().postForEntity(s2sUrl + "/lease", signInDetails, String.class).getBody();
     }
 }
