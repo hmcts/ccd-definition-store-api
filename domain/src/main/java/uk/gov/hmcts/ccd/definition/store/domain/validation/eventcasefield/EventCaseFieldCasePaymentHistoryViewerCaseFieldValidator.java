ffb7668f95f76a8c7d8dea00f69d18dee2a5f4b1
andrewf <andrzej.folga@hmcts.net>
2018-06-18 14:29:59 +0100

    RDM-2314 Use enum reference for Base Case Payment History.

--- domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/validation/eventcasefield/EventCaseFieldCasePaymentHistoryViewerCaseFieldValidator.java
+++ domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/validation/eventcasefield/EventCaseFieldCasePaymentHistoryViewerCaseFieldValidator.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.ccd.definition.store.domain.validation.eventcasefield;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.definition.store.domain.validation.ValidationResult;
 import uk.gov.hmcts.ccd.definition.store.repository.DisplayContext;
+import uk.gov.hmcts.ccd.definition.store.repository.FieldTypeUtils;
 import uk.gov.hmcts.ccd.definition.store.repository.entity.EventCaseFieldEntity;
 
 @Component
@@ -13,7 +14,7 @@ public class EventCaseFieldCasePaymentHistoryViewerCaseFieldValidator implements
                                      EventCaseFieldEntityValidationContext eventCaseFieldEntityValidationContext) {
         ValidationResult validationResult = new ValidationResult();
 
-        if ("CasePaymentHistoryViewer".equals(eventCaseFieldEntity.getCaseField().getFieldType().getReference())
+        if (FieldTypeUtils.BASE_CASE_PAYMENT_HISTORY_VIEWER.equals(eventCaseFieldEntity.getCaseField().getFieldType().getReference())
                 && null != eventCaseFieldEntity.getDisplayContext()
                 && !eventCaseFieldEntity.getDisplayContext().equals(DisplayContext.READONLY)) {
             validationResult.addError(
