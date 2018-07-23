557af04a5bf32e3dccc91859810e42aa5e70ff19
Lam <daniel.2.lam@atos.net>
2018-07-23 14:00:01 +0100

    Remove redundant imports and amend class description

--- repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/model/StateLite.java
+++ repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/model/StateLite.java
@@ -1,16 +1,10 @@
 package uk.gov.hmcts.ccd.definition.store.repository.model;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.util.Objects;
-
 /**
- * A "lite" version of the {@link uk.gov.hmcts.ccd.definition.store.repository.entity.StateEntity} class that contains selected State fields (reference and id) for
- * display purposes. (Class introduced to break a circular dependency between Jurisdiction (containing a list of
- * states) and states (containing a reference to a parent CaseType), when using the Mapstruct mapper
- * interface.)
+ * A "lite" version of the {@link uk.gov.hmcts.ccd.definition.store.repository.entity.StateEntity} class that contains
+ * selected State fields (reference and id) for display purposes. (Class introduced to break a circular dependency
+ * between CaseType (containing a list of States) and State (containing a reference to a parent CaseType), when using
+ * the Mapstruct mapper interface.)
  */
 public class StateLite {
     private String id = null;
@@ -31,5 +25,4 @@ public class StateLite {
     public void setName(String name) {
         this.name = name;
     }
-
 }
