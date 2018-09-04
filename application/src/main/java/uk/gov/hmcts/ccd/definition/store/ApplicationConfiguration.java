59dece1ed72063b7cff82eed3a880b3007946b4a
mariopaniccia <Mario.Paniccia@HMCTS.NET>
2018-09-04 08:23:50 +0100

    made async exec props configurable as properties

--- application/src/main/java/uk/gov/hmcts/ccd/definition/store/ApplicationConfiguration.java
+++ application/src/main/java/uk/gov/hmcts/ccd/definition/store/ApplicationConfiguration.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.ccd.definition.store;
 
 import java.util.concurrent.Executor;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,6 +13,18 @@ import org.springframework.web.client.RestTemplate;
 @EnableAsync
 class ApplicationConfiguration {
 
+    @Value("${async.executor.core.pool.size}")
+    private Integer corePoolSize;
+
+    @Value("${async.executor.max.pool.size}")
+    private Integer maxPoolSize;
+
+    @Value("${async.executor.queue.capacity}")
+    private Integer queueCapacity;
+
+    @Value("${async.executor.prefix}")
+    private String prefix;
+
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
@@ -20,10 +33,10 @@ class ApplicationConfiguration {
     @Bean
     public Executor asyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(500);
-        executor.setThreadNamePrefix("async-exec-");
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix(prefix);
         executor.initialize();
         return executor;
     }
