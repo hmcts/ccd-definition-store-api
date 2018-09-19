788fb5642cd9bee211a47a3527b70ff8b38331cf
Dwayne Bailey <dwayne.bailey@hmcts.net>
2018-09-19 21:49:37 +0100

    Remove unused imports
    Remove unused imports detected by checkstyle UnusedImportsCheck
    
    RDM-2604

--- repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/entity/SearchInputCaseFieldEntity.java
+++ repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/entity/SearchInputCaseFieldEntity.java
@@ -1,11 +1,6 @@
 package uk.gov.hmcts.ccd.definition.store.repository.entity;
 
 import javax.persistence.*;
-import java.io.Serializable;
-import java.time.LocalDate;
-
-import static javax.persistence.FetchType.EAGER;
-import static javax.persistence.FetchType.LAZY;
 
 @Table(name = "search_input_case_field")
 @Entity
