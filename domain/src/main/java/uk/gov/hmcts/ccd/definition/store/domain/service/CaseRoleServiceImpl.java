29788d56a3476f1beba6fc633caa4706c22ae3ec
tzarmango <fatih.ozceylan@gmail.com>
2018-09-24 22:31:58 +0100

    RDM-2835 fix for 'HTTP-404 not thrown on invalid case-type'

--- domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/service/CaseRoleServiceImpl.java
+++ domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/service/CaseRoleServiceImpl.java
@@ -1,29 +1,36 @@
 package uk.gov.hmcts.ccd.definition.store.domain.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.definition.store.domain.exception.NotFoundException;
+import uk.gov.hmcts.ccd.definition.store.repository.CaseRoleRepository;
+import uk.gov.hmcts.ccd.definition.store.repository.CaseTypeRepository;
+import uk.gov.hmcts.ccd.definition.store.repository.entity.CaseRoleEntity;
+import uk.gov.hmcts.ccd.definition.store.repository.model.CaseRole;
+
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import uk.gov.hmcts.ccd.definition.store.repository.CaseRoleRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.entity.CaseRoleEntity;
-import uk.gov.hmcts.ccd.definition.store.repository.model.CaseRole;
-
 @Component
 public class CaseRoleServiceImpl implements CaseRoleService {
     private final CaseRoleRepository caseRoleRepository;
+    private final CaseTypeRepository caseTypeRepository;
+
     private final EntityToResponseDTOMapper dtoMapper;
 
     @Autowired
     public CaseRoleServiceImpl(CaseRoleRepository caseRoleRepository,
-                               EntityToResponseDTOMapper dtoMapper) {
+                               CaseTypeRepository caseTypeRepository, EntityToResponseDTOMapper dtoMapper) {
         this.caseRoleRepository = caseRoleRepository;
+        this.caseTypeRepository = caseTypeRepository;
         this.dtoMapper = dtoMapper;
     }
 
     @Override
     public List<CaseRole> findByCaseTypeId(String caseType) {
+        caseTypeRepository.findLastVersion(caseType).orElseThrow(() -> new NotFoundException(caseType));
+
         List<CaseRoleEntity> caseRoleEntities = caseRoleRepository.findCaseRoleEntitiesByCaseType(caseType);
 
         return caseRoleEntities
--- domain/src/test/java/uk/gov/hmcts/ccd/definition/store/domain/service/CaseRoleServiceImplTest.java
+++ domain/src/test/java/uk/gov/hmcts/ccd/definition/store/domain/service/CaseRoleServiceImplTest.java
@@ -5,17 +5,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import uk.gov.hmcts.ccd.definition.store.domain.exception.NotFoundException;
 import uk.gov.hmcts.ccd.definition.store.repository.CaseRoleRepository;
+import uk.gov.hmcts.ccd.definition.store.repository.CaseTypeRepository;
 import uk.gov.hmcts.ccd.definition.store.repository.entity.CaseRoleEntity;
 import uk.gov.hmcts.ccd.definition.store.repository.model.CaseRole;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 class CaseRoleServiceImplTest {
     private final String CASE_TYPE_REFERENCE = "Case Type 1";
@@ -23,6 +28,8 @@ class CaseRoleServiceImplTest {
     EntityToResponseDTOMapper dtoMapper;
     @Mock
     private CaseRoleRepository caseRoleRepository;
+    @Mock
+    private CaseTypeRepository caseTypeRepository;
 
     private CaseRoleServiceImpl classUnderTest;
     private CaseRoleEntity caseRoleEntity1 = new CaseRoleEntity();
@@ -35,7 +42,7 @@ class CaseRoleServiceImplTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
-        classUnderTest = new CaseRoleServiceImpl(caseRoleRepository, dtoMapper);
+        classUnderTest = new CaseRoleServiceImpl(caseRoleRepository, caseTypeRepository, dtoMapper);
         caseRoleEntity1.setReference("ref1");
         caseRole1.setId(caseRoleEntity1.getReference());
         doReturn(Arrays.asList(caseRoleEntity1, caseRoleEntity2, caseRoleEntity3))
@@ -49,11 +56,23 @@ class CaseRoleServiceImplTest {
     @DisplayName("should return Case Role List")
     @Test
     void findByCaseTypeId() {
+        doReturn(Optional.of(1)).when(caseTypeRepository).findLastVersion(CASE_TYPE_REFERENCE);
+
         final List<CaseRole> caseRoles = classUnderTest.findByCaseTypeId(CASE_TYPE_REFERENCE);
 
         assertAll(
             () -> assertThat(caseRoles.size(), is(3)),
             () -> assertThat(caseRoles.get(0).getId(), is(caseRoleEntity1.getReference()))
         );
+    }
+
+    @DisplayName("should throw exception when case type not found")
+    @Test
+    void invalidCaseTypeId() {
+        doThrow(new NotFoundException("Not found")).when(caseTypeRepository).findLastVersion(CASE_TYPE_REFERENCE);
+
+        assertThrows(NotFoundException.class, () -> {
+            final List<CaseRole> caseRoles = classUnderTest.findByCaseTypeId(CASE_TYPE_REFERENCE);
+        });
     }
 }
