b6e5a27f1f0c750d5aea30ca06b519382e8ad6e5
Lam <daniel.2.lam@atos.net>
2018-09-27 16:30:01 +0100

    Remove unused imports

--- excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/azurestorage/service/FileStorageClient.java
+++ excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/azurestorage/service/FileStorageClient.java
@@ -3,9 +3,6 @@ package uk.gov.hmcts.ccd.definition.store.excel.azurestorage.service;
 import org.springframework.web.multipart.MultipartFile;
 import uk.gov.hmcts.ccd.definition.store.excel.domain.definition.model.DefinitionFileUploadMetadata;
 
-import java.io.OutputStream;
-import java.util.UUID;
-
 public interface FileStorageClient {
 
     void uploadFile(MultipartFile file, DefinitionFileUploadMetadata metadata);
