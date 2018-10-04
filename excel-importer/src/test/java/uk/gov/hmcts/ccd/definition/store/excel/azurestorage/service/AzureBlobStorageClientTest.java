9d9f3a3a336a6eb8785132babb5406dad9cf1bbe
Lam <daniel.2.lam@atos.net>
2018-10-04 13:50:13 +0100

    Remove unused import

--- excel-importer/src/test/java/uk/gov/hmcts/ccd/definition/store/excel/azurestorage/service/AzureBlobStorageClientTest.java
+++ excel-importer/src/test/java/uk/gov/hmcts/ccd/definition/store/excel/azurestorage/service/AzureBlobStorageClientTest.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyLong;
 import static org.mockito.Mockito.mock;
