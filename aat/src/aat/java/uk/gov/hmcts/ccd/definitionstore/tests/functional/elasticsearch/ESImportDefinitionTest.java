3769fb20a96afde126db24344a5abf14166fd29a
gbenadikar <giri.benadikar@hmcts.net>
2018-10-08 14:52:23 +0100

    RDM-2814 - functional test for Elasticsearch mapper on definition import

--- aat/src/aat/java/uk/gov/hmcts/ccd/definitionstore/tests/functional/elasticsearch/ESImportDefinitionTest.java
+++ aat/src/aat/java/uk/gov/hmcts/ccd/definitionstore/tests/functional/elasticsearch/ESImportDefinitionTest.java
@@ -11,11 +11,13 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.ccd.definitionstore.tests.AATHelper;
 import uk.gov.hmcts.ccd.definitionstore.tests.BaseTest;
@@ -49,30 +51,78 @@ class ESImportDefinitionTest extends BaseTest {
             .body("acknowledged", equalTo(true));
     }
 
-    @Test
-    @DisplayName("Should import a valid definition multiple times and create Elasticsearch index, alias and field mappings")
-    void shouldImportValidDefinitionMultipleTimes() {
-        // invoke definition import
-        asAutoTestImporter().get()
-            .given()
-            .multiPart(new File("src/resource/CCD_TEST_ES.xlsx"))
-            .expect()
-            .statusCode(201)
-            .when()
-            .post("/import");
+    @Nested
+    @DisplayName("successful run")
+    class SuccessfulRun {
+        @Test
+        @DisplayName("Should import a valid definition multiple times and create Elasticsearch index, alias and field mappings")
+        void shouldImportValidDefinitionMultipleTimes() {
+            // invoke definition import
+            asAutoTestImporter().get()
+                .given()
+                .multiPart(new File("src/resource/CCD_TEST_ES.xlsx"))
+                .expect()
+                .statusCode(201)
+                .when()
+                .post("/import");
 
-        verifyIndexAndFieldMappings(false);
+            verifyIndexAndFieldMappings(false);
 
-        // invoke definition import second time
-        asAutoTestImporter().get()
-            .given()
-            .multiPart(new File("src/resource/CCD_TEST_ES_WithNewField.xlsx"))
-            .expect()
-            .statusCode(201)
-            .when()
-            .post("/import");
+            // invoke definition import second time
+            asAutoTestImporter().get()
+                .given()
+                .multiPart(new File("src/resource/CCD_TEST_ES_WithNewField.xlsx"))
+                .expect()
+                .statusCode(201)
+                .when()
+                .post("/import");
 
-        verifyIndexAndFieldMappings(true);
+            verifyIndexAndFieldMappings(true);
+        }
+    }
+
+    @Nested
+    @DisplayName("Failed run")
+    class FailedRun {
+        @Test
+        @DisplayName("Should not import definition when Elasticsearch throws exception")
+        void shouldNotImportDefinitionOnEsError() {
+            // invoke definition import
+            asAutoTestImporter().get()
+                .given()
+                .multiPart(new File("src/resource/CCD_TEST_ES.xlsx"))
+                .expect()
+                .statusCode(201)
+                .when()
+                .post("/import");
+
+            verifyIndexAndFieldMappings(false);
+
+            removeAlias();
+
+            // invoking definition import second time should throw exception as alias does not exist
+            asAutoTestImporter().get()
+                .given()
+                .multiPart(new File("src/resource/CCD_TEST_ES_WithNewField.xlsx"))
+                .expect()
+                .statusCode(503)
+                .when()
+                .post("/import");
+        }
+
+        private void removeAlias() {
+            // deletes the index
+            asElasticsearchApiUser()
+                .given()
+                .contentType(ContentType.JSON)
+                .body("{\"actions\":[{\"remove\":{\"index\":\"es_test_cases-000001\",\"alias\":\"es_test_cases\"}}]}")
+                .when()
+                .post("_aliases")
+                .then()
+                .statusCode(200)
+                .body("acknowledged", equalTo(true));
+
+        }
     }
 
     private void verifyIndexAndFieldMappings(boolean verifyNewFields) {
@@ -99,12 +149,6 @@ class ESImportDefinitionTest extends BaseTest {
         }
     }
 
-    private RequestSpecification asElasticsearchApiUser() {
-        return RestAssured.given(new RequestSpecBuilder()
-                                     .setBaseUri(aat.getElasticsearchBaseUri())
-                                     .build());
-    }
-
     private void verifyIndexAndAlias(ValidatableResponse responseBody) {
         responseBody
             .body("", hasKey(CASE_INDEX_NAME))
@@ -118,15 +162,19 @@ class ESImportDefinitionTest extends BaseTest {
     }
 
     private void verifyFieldsAndType(ValidatableResponse responseBody, String type, String... fields) {
-        Stream.of(fields).forEach(field ->
-                                      responseBody
+        Stream.of(fields).forEach(field -> responseBody
                                           .body("", hasKey(field))
-                                          .body(field + ".type", is(type))
-        );
+                                          .body(field + ".type", is(type)));
     }
 
     private void verifyFieldsDoNotOccurInResponse(ValidatableResponse responseBody, String... fields) {
         Stream.of(fields).forEach(field -> responseBody.body("", not(hasKey(field))));
+    }
+
+    private RequestSpecification asElasticsearchApiUser() {
+        return RestAssured.given(new RequestSpecBuilder()
+                                     .setBaseUri(aat.getElasticsearchBaseUri())
+                                     .build());
     }
 
 }
