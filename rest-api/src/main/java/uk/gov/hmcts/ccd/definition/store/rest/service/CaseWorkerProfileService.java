84ce54137631fa858f7376a75daf0005c43737dc
Adrian Cho <waihung.cho@hmcts.net>
2018-11-19 16:00:43 +0000

    RDM-2425

---
+++ rest-api/src/main/java/uk/gov/hmcts/ccd/definition/store/rest/service/CaseWorkerProfileService.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.ccd.definition.store.rest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.ccd.definition.store.domain.ApplicationParams;
+import uk.gov.hmcts.ccd.definition.store.repository.SecurityUtils;
+import uk.gov.hmcts.ccd.definition.store.rest.model.IDAMProperties;
+import uk.gov.hmcts.reform.auth.checker.spring.serviceanduser.ServiceAndUserDetails;
+
+import static org.springframework.http.HttpMethod.GET;
+
+@Service
+public class CaseWorkerProfileService {
+
+    private final SecurityUtils securityUtils;
+    private final RestTemplate restTemplate;
+    private ApplicationParams applicationParams;
+
+    @Autowired
+    CaseWorkerProfileService(final SecurityUtils securityUtils,
+                                        final RestTemplate restTemplate,
+                                        final ApplicationParams applicationParams) {
+
+        this.securityUtils = securityUtils;
+        this.restTemplate = restTemplate;
+        this.applicationParams = applicationParams;
+    }
+
+    public IDAMProperties getLoggedInUserDetails() {
+        final HttpEntity<ServiceAndUserDetails>
+            requestEntity =
+            new HttpEntity<>(securityUtils.userAuthorizationHeaders());
+        return restTemplate.exchange(applicationParams.idamUserProfileURL(), GET, requestEntity, IDAMProperties.class)
+                           .getBody();
+    }
+
+}
