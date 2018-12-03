0f90977d9b564557fea3d3d2bd6676fc630a9876
Adrian Cho <waihung.cho@hmcts.net>
2018-12-03 14:27:08 +0000

    RDM-1956

--- rest-api/src/main/java/uk/gov/hmcts/ccd/definition/store/rest/endpoint/ImportAuditController.java
+++ rest-api/src/main/java/uk/gov/hmcts/ccd/definition/store/rest/endpoint/ImportAuditController.java
@@ -2,8 +2,12 @@ package uk.gov.hmcts.ccd.definition.store.rest.endpoint;
 
 import com.microsoft.azure.storage.StorageException;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.ccd.definition.store.rest.model.ImportAudit;
 import uk.gov.hmcts.ccd.definition.store.rest.service.AzureBlobStorageClient;
@@ -14,7 +18,7 @@ import static java.util.Collections.emptyList;
 
 @RestController
 @Api(value = "/api/import-audits")
-@RequestMapping(value = "/api/import-audits")
+@RequestMapping(value = "/api")
 class ImportAuditController {
 
     private final AzureBlobStorageClient azureBlobStorageClient;
@@ -24,6 +28,11 @@ class ImportAuditController {
         this.azureBlobStorageClient = azureBlobStorageClient;
     }
 
+    @RequestMapping(value = "/import-audits", method = RequestMethod.GET, produces = {"application/json"})
+    @ApiOperation(value = "Fetches import audits")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Import audits")
+    })
     Collection<ImportAudit> fetchAllAudits() throws StorageException {
         if (null != azureBlobStorageClient) {
             return azureBlobStorageClient.fetchAllAudits();
