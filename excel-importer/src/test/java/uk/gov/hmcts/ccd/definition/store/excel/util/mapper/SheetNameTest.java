d1c2bcb686e569612d5dd6b50806d8ecd0f98a26
Adrian Cho <waihung.cho@hmcts.net>
2019-01-18 11:54:00 +0000

    Unrelated to RDM-3187 but addressing drop in test coverage

---
+++ excel-importer/src/test/java/uk/gov/hmcts/ccd/definition/store/excel/util/mapper/SheetNameTest.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.ccd.definition.store.excel.util.mapper;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class SheetNameTest {
+
+    @Test
+    @DisplayName("Should return null when Sheet name is not found")
+    void shouldReturnNullWhenSheetNameIsNotFound() {
+        assertNull(SheetName.forName("SheetNameNotExpectHere"));
+    }
+
+    @Test
+    @DisplayName("Should return a sheet when matching name is found")
+    void shouldReturnSheetWhenMatchingNameIsFound() {} {
+        final SheetName[] sheetNames = SheetName.values();
+        for (SheetName s: sheetNames) {
+            assertThat(SheetName.forName(s.getName()), is(s));
+        }
+    }
+}
