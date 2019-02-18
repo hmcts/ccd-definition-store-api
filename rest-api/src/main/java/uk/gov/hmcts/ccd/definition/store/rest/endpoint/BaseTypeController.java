a5ffa52f76a6ed2f06d153b657a403e96676de26
Adrian Cho <waihung.cho@hmcts.net>
2019-02-18 15:30:01 +0000

    RDM-4021 Spike

--- rest-api/src/main/java/uk/gov/hmcts/ccd/definition/store/rest/endpoint/BaseTypeController.java
+++ rest-api/src/main/java/uk/gov/hmcts/ccd/definition/store/rest/endpoint/BaseTypeController.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +15,7 @@ import uk.gov.hmcts.ccd.definition.store.repository.FieldTypeRepository;
 import uk.gov.hmcts.ccd.definition.store.repository.entity.FieldTypeEntity;
 import uk.gov.hmcts.ccd.definition.store.repository.model.FieldType;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,15 +26,19 @@ import java.util.stream.Collectors;
 public class BaseTypeController {
 
     private EntityToResponseDTOMapper entityToResponseDTOMapper;
+    private final HttpServletRequest httpServletRequest;
 
     private List<FieldTypeEntity> baseTypes;
+
+    private static final Logger LOG = LoggerFactory.getLogger(BaseTypeController.class);
 
     @Autowired
     public BaseTypeController(FieldTypeRepository fieldTypeRepository,
                               EntityToResponseDTOMapper entityToResponseDTOMapper,
-                              HttpServletRequest context) {
+                              HttpServletRequest httpServletRequest) {
         this.baseTypes = fieldTypeRepository.findCurrentBaseTypes();
         this.entityToResponseDTOMapper = entityToResponseDTOMapper;
+        this.httpServletRequest = httpServletRequest;
     }
 
     @RequestMapping(value = "/base-types", method = RequestMethod.GET, produces = {"application/json"})
@@ -40,6 +47,11 @@ public class BaseTypeController {
         @ApiResponse(code = 200, message = "All valid base types")
     })
     public List<FieldType> getBaseTypes(HttpServletRequest request) {
+        LOG.warn("httpServletRequest.getAuthType() {}", httpServletRequest.getAuthType());
+        LOG.warn("{}", httpServletRequest.getCookies());
+        for (Cookie c : httpServletRequest.getCookies()) {
+            LOG.warn("name {} value {}", c.getName(), c.getValue());
+        }
         return baseTypes.stream().map(entityToResponseDTOMapper::map).collect(Collectors.toList());
     }
 }
