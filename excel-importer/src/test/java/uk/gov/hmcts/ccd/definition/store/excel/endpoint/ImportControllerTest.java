23f6e7edc0910548f864f93fbee6d2f5b46d534b
Lam <daniel.2.lam@atos.net>
2019-05-09 10:30:05 +0100

    RDM-4265: DefaultHidden column warning on Definition import
    Return warnings in custom response header if the legacy `DefaultHidden` column is found on any spreadsheet tabs, during import processing.

--- excel-importer/src/test/java/uk/gov/hmcts/ccd/definition/store/excel/endpoint/ImportControllerTest.java
+++ excel-importer/src/test/java/uk/gov/hmcts/ccd/definition/store/excel/endpoint/ImportControllerTest.java
@@ -3,16 +3,22 @@ package uk.gov.hmcts.ccd.definition.store.excel.endpoint;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.willThrow;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.ccd.definition.store.excel.endpoint.ImportController.IMPORT_WARNINGS_HEADER;
 import static uk.gov.hmcts.ccd.definition.store.excel.endpoint.ImportController.URI_IMPORT;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -58,13 +64,14 @@ class ImportControllerTest {
         metadata.addCaseType("TestCaseType");
         metadata.setUserId("user@hmcts.net");
         when(importService.importFormDefinitions(any())).thenReturn(metadata);
+        when(importService.getImportWarnings()).thenReturn(Collections.emptyList());
     }
 
     @DisplayName("Upload - Green path, Azure enabled")
     @Test
     void validUploadAzureEnabled() throws Exception {
         when(azureStorageConfiguration.isAzureUploadEnabled()).thenReturn(true);
-        mockMvc.perform(fileUpload(URI_IMPORT).file(file))
+        mockMvc.perform(multipart(URI_IMPORT).file(file))
                .andExpect(status().isCreated())
                .andExpect(content().string("Case Definition data successfully imported"));
         verify(fileStorageService).uploadFile(file, metadata);
@@ -74,7 +81,7 @@ class ImportControllerTest {
     @Test
     void validUploadAzureDisabled() throws Exception {
         when(azureStorageConfiguration.isAzureUploadEnabled()).thenReturn(false);
-        mockMvc.perform(fileUpload(URI_IMPORT).file(file))
+        mockMvc.perform(multipart(URI_IMPORT).file(file))
             .andExpect(status().isCreated())
             .andExpect(content().string("Case Definition data successfully imported"));
         verify(fileStorageService, never()).uploadFile(file, metadata);
@@ -88,7 +95,20 @@ class ImportControllerTest {
 
         final IOException
             exception =
-            assertThrows(IOException.class, () -> mockMvc.perform(fileUpload(URI_IMPORT).file(file)));
+            assertThrows(IOException.class, () -> mockMvc.perform(multipart(URI_IMPORT).file(file)));
         assertThat(exception.getMessage(), is("boo"));
+    }
+
+    @DisplayName("Upload - warnings during import, Azure disabled")
+    @Test
+    void validUploadWithWarnings() throws Exception {
+        when(azureStorageConfiguration.isAzureUploadEnabled()).thenReturn(false);
+        final String firstWarning = "First warning";
+        final String secondWarning = "Second warning";
+        when(importService.getImportWarnings()).thenReturn(Arrays.asList(firstWarning, secondWarning));
+        mockMvc.perform(multipart(URI_IMPORT).file(file))
+            .andExpect(status().isCreated())
+            .andExpect(content().string("Case Definition data successfully imported"))
+            .andExpect(header().stringValues(IMPORT_WARNINGS_HEADER, firstWarning, secondWarning));
     }
 }
