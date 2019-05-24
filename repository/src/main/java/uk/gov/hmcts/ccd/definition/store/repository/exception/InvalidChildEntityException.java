238b8f3bb4f650ab1740c73a9826529f171de8f4
andrewf <andrzej.folga@hmcts.net>
2019-05-24 08:43:55 +0100

    RDM-2987 Remove unused exceptions.

--- repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/exception/InvalidChildEntityException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmcts.ccd.definition.store.repository.exception;
-
-public class InvalidChildEntityException extends RuntimeException {
-    public InvalidChildEntityException(String message) {
-        super(message);
-    }
-}
--- repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/exception/NonSimplePlaceholderLeafEntityException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmcts.ccd.definition.store.repository.exception;
-
-public class NonSimplePlaceholderLeafEntityException extends RuntimeException {
-    public NonSimplePlaceholderLeafEntityException(String message) {
-        super(message);
-    }
-}
