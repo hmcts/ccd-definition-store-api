e99f8faaa728fe17a1918733c911e6138fa51d9d
Sateesh Mathangi <a-smathangi@hotels.com>
2019-06-04 14:42:30 +0100

    RDM-4881 - import case first and then get

--- aat/src/aat/java/uk/gov/hmcts/ccd/definitionstore/tests/functional/CaseTypeTest.java
+++ aat/src/aat/java/uk/gov/hmcts/ccd/definitionstore/tests/functional/CaseTypeTest.java
@@ -1,9 +1,11 @@
 package uk.gov.hmcts.ccd.definitionstore.tests.functional;
 
+import java.io.File;
 import java.util.function.Supplier;
 
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.ccd.definitionstore.tests.AATHelper;
@@ -21,6 +23,17 @@ class CaseTypeTest extends BaseTest {
     Supplier<RequestSpecification> asUserWithUser = asAutoTestCaseworkerWithUser();
     Supplier<RequestSpecification> asUser = asAutoTestCaseworker();
 
+    @BeforeAll
+    void setUp() {
+        Supplier<RequestSpecification> asTestImporter = asAutoTestImporter();
+        asTestImporter.get()
+            .given()
+            .multiPart(new File("src/resource/CCD_CNP_27.xlsx"))
+            .expect()
+            .statusCode(201)
+            .when()
+            .post("/import");
+    }
 
     @Test
     @DisplayName("Should return case type definition")
