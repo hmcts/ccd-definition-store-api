6b4f6aab2eb0195f6c93b6a457f8897aba5f86fc
mutlu <mutlu.sancaktutar@hmcts.net>
2019-08-14 09:45:32 +0100

    remove blank lines

--- domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/AmPersistenceReadSource.java
+++ domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/AmPersistenceReadSource.java
@@ -2,5 +2,4 @@ package uk.gov.hmcts.ccd.definition.store.domain;
 
 public enum AmPersistenceReadSource {
     FROM_CCD, FROM_AM
-
 }
--- domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/AmPersistenceWriteDestination.java
+++ domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/AmPersistenceWriteDestination.java
@@ -2,5 +2,4 @@ package uk.gov.hmcts.ccd.definition.store.domain;
 
 public enum AmPersistenceWriteDestination {
     TO_CCD, TO_AM, TO_BOTH
-
 }
