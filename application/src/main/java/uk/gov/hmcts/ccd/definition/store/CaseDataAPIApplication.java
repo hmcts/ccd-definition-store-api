8471a7fd2e1676ff4feb44d989c42d1fd77d93bb
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-11-14 23:41:53 +0000

    Moving to hmcts version of liquibase which performs better DB locking

--- application/src/main/java/uk/gov/hmcts/ccd/definition/store/CaseDataAPIApplication.java
+++ application/src/main/java/uk/gov/hmcts/ccd/definition/store/CaseDataAPIApplication.java
@@ -1,11 +1,17 @@
 package uk.gov.hmcts.ccd.definition.store;
 
+import liquibase.configuration.GlobalConfiguration;
+import liquibase.configuration.LiquibaseConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CaseDataAPIApplication {
     public static void main(String[] args) {
+        //Setting Liquibase DB Lock property before Spring starts up.
+        LiquibaseConfiguration.getInstance()
+            .getConfiguration(GlobalConfiguration.class)
+            .setUseDbLock(true);
         SpringApplication.run(CaseDataAPIApplication.class, args);
     }
 }
