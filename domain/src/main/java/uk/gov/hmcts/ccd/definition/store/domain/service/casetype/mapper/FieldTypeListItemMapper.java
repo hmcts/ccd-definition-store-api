1e955652fc06a38e9a8b27271b018c7d6b45d802
Tobias <it.consulting@tutanota.com>
2019-11-19 14:31:12 +0000

    RDM-5735: Fixed list display order should be ASC, not DESC
          [RDM-5735](https://tools.hmcts.net/jira/browse/RDM-5735).

--- domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/service/casetype/mapper/FieldTypeListItemMapper.java
+++ domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/service/casetype/mapper/FieldTypeListItemMapper.java
@@ -5,25 +5,43 @@ import uk.gov.hmcts.ccd.definition.store.repository.model.FixedListItem;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FieldTypeListItemMapper {
     public List<FixedListItem> entityToModel(final List<FieldTypeListItemEntity>
-                                                                    fieldTypeListItemEntities) {
+                                                 fieldTypeListItemEntities) {
         List<FixedListItem> fixedListItems = new ArrayList<>();
         fieldTypeListItemEntities.stream().forEach(fieldTypeListItemEntity ->
-                                                   {
-                                                       FixedListItem fixedListItem = new FixedListItem();
-                                                       fixedListItem
-                                                           .setCode(fieldTypeListItemEntity.getValue());
-                                                       fixedListItem
-                                                           .setLabel(fieldTypeListItemEntity.getLabel());
-                                                       fixedListItem
-                                                           .setOrder(fieldTypeListItemEntity.getOrder());
-                                                       fixedListItems.add(fixedListItem);
-                                                   }
-                                                  );
+            {
+                FixedListItem fixedListItem = new FixedListItem();
+                fixedListItem
+                    .setCode(fieldTypeListItemEntity.getValue());
+                fixedListItem
+                    .setLabel(fieldTypeListItemEntity.getLabel());
+                fixedListItem
+                    .setOrder(fieldTypeListItemEntity.getOrder());
+                fixedListItems.add(fixedListItem);
+            }
+        );
+        if (hasADisplayOrder(fixedListItems)) {
+            return applyDisplayOrder(fixedListItems);
+        }
         return fixedListItems;
     }
 
 
+    private List<FixedListItem> applyDisplayOrder(final List<FixedListItem> fixedListItems) {
+
+        return fixedListItems.stream()
+            .sorted((fixedListItem1, fixedListItem2) ->
+                fixedListItem1.getOrder().compareTo(fixedListItem2.getOrder()
+                )).collect(Collectors.toList());
+    }
+
+    private boolean hasADisplayOrder(final List<FixedListItem> fixedListItems) {
+
+        final long result = fixedListItems.stream().filter(fixedListItem -> fixedListItem.getOrder() != null).count();
+        //if all elements has an order, it means that the order count has to the same as the list size.
+        return result == fixedListItems.size();
+    }
 }
