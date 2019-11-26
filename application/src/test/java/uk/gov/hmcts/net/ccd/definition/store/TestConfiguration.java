7d06909eee18e09b449375c5d55d231e980c3eca
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2019-11-26 11:46:07 +0000

    Moving to testcontainers from embedded postgres (otj-pg-embedded). otj-pg-embedded is not compatible with latest liquibase-core version as it fails to create sequence for column type "serial".

--- application/src/test/java/uk/gov/hmcts/net/ccd/definition/store/TestConfiguration.java
+++ application/src/test/java/uk/gov/hmcts/net/ccd/definition/store/TestConfiguration.java
@@ -1,14 +1,6 @@
 package uk.gov.hmcts.net.ccd.definition.store;
 
-import javax.annotation.PreDestroy;
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.concurrent.ThreadLocalRandom;
-
-import com.opentable.db.postgres.embedded.EmbeddedPostgres;
 import org.mockito.Mockito;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,33 +10,10 @@ import org.springframework.core.io.support.ResourcePropertySource;
 import org.springframework.web.context.ContextCleanupListener;
 import uk.gov.hmcts.ccd.definition.store.excel.service.ImportServiceImpl;
 
+import java.io.IOException;
+
 @Configuration
 public class TestConfiguration extends ContextCleanupListener {
-
-    private static final Logger LOG = LoggerFactory.getLogger(TestConfiguration.class);
-
-    @Bean
-    public EmbeddedPostgres embeddedPostgres() throws IOException {
-
-        int port = randomPort();
-
-        LOG.info("Starting postgres with port number {}", port);
-
-        return EmbeddedPostgres
-            .builder()
-            .setPort(port)
-            .start();
-    }
-
-    @Bean
-    public DataSource dataSource() throws IOException {
-        return embeddedPostgres().getPostgresDatabase();
-    }
-
-    @PreDestroy
-    public void contextDestroyed() throws IOException {
-        embeddedPostgres().close();
-    }
 
     @Autowired
     public void loadLocalOverridePropertiesIfItExists(StandardEnvironment environment) {
@@ -61,9 +30,5 @@ public class TestConfiguration extends ContextCleanupListener {
     public ImportServiceImpl importServiceSpy(ImportServiceImpl importService) {
         // Use a spy (which is autowired in BaseTest), so specific methods can be overridden
         return Mockito.spy(importService);
-    }
-
-    private int randomPort() {
-        return ThreadLocalRandom.current().nextInt(50366, 60366);
     }
 }
--- repository/src/test/java/uk/gov/hmcts/ccd/definition/store/repository/TestConfiguration.java
+++ repository/src/test/java/uk/gov/hmcts/ccd/definition/store/repository/TestConfiguration.java
@@ -1,36 +1,11 @@
 package uk.gov.hmcts.ccd.definition.store.repository;
 
-import com.opentable.db.postgres.embedded.EmbeddedPostgres;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.concurrent.ThreadLocalRandom;
 
 @Configuration
 public class TestConfiguration {
 
-    private static final Logger LOG = LoggerFactory.getLogger(TestConfiguration.class);
+    public void emptyConfig() {
 
-    @Bean
-    public DataSource dataSource() throws IOException {
-
-        int port = randomPort();
-
-        LOG.info("Starting postgres with port number {}", port);
-
-        final EmbeddedPostgres pg = EmbeddedPostgres
-            .builder()
-            .setPort(randomPort())
-            .start();
-
-        return pg.getPostgresDatabase();
-    }
-
-    private int randomPort() {
-        return ThreadLocalRandom.current().nextInt(50366, 60366);
     }
 }
