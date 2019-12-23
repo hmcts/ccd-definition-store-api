63bd216c1d5ee5f1d6a9c78880e7f0d6dc6fb64c
kiran.yenigala@hmcts.net <Yrlks1091327~>
2019-12-23 13:40:43 +0000

    RDM-6719: Add Banner Feature to CCD UI

--- domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/service/banner/BannerService.java
+++ domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/service/banner/BannerService.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.ccd.definition.store.domain.service.banner;
 import java.util.List;
 import uk.gov.hmcts.ccd.definition.store.repository.entity.BannerEntity;
 import uk.gov.hmcts.ccd.definition.store.repository.model.Banner;
-import uk.gov.hmcts.ccd.definition.store.repository.model.Jurisdiction;
 
 public interface BannerService {
 
