ebc81044ea09c0070e6306acce7064d1455c1481
Dan Lysiak <daniel.lysiak@cgi.com>
2020-01-08 13:55:27 +0000

    Re-add missing import

--- rest-api/src/test/java/uk/gov/hmcts/ccd/definition/store/rest/endpoint/DisplayApiControllerTest.java
+++ rest-api/src/test/java/uk/gov/hmcts/ccd/definition/store/rest/endpoint/DisplayApiControllerTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.ccd.definition.store.rest.endpoint;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.times;
