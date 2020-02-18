9832336bed6689a2f6d0c99617f2e3c9836fd97b
mutlu <mutlu.sancaktutar@hmcts.net>
2020-02-18 23:43:25 +0000

    remove redundant import

--- aat/src/aat/java/uk/gov/hmcts/ccd/definitionstore/tests/DataSetupExtension.java
+++ aat/src/aat/java/uk/gov/hmcts/ccd/definitionstore/tests/DataSetupExtension.java
@@ -1,17 +1,16 @@
 package uk.gov.hmcts.ccd.definitionstore.tests;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import org.apache.commons.lang.BooleanUtils;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import uk.gov.hmcts.ccd.definitionstore.tests.helper.idam.AuthenticatedUser;
-
-import java.io.File;
-
 import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
+
+import org.apache.commons.lang.BooleanUtils;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import uk.gov.hmcts.ccd.definitionstore.tests.helper.idam.AuthenticatedUser;
 
 /**
  * Custom Extension which executes setup code only once before all tests are started.
@@ -36,23 +35,6 @@ public class DataSetupExtension implements BeforeAllCallback {
         String s2sToken = aat.getS2SHelper().getToken();
 
         createTestRole(aat, s2sToken);
-
-        importDefinitions(aat, s2sToken);
-    }
-
-    private void importDefinitions(AATHelper aat, String s2sToken) {
-        AuthenticatedUser importer = aat.getIdamHelper()
-            .authenticate(aat.getImporterAutoTestEmail(),
-                aat.getImporterAutoTestPassword());
-
-        RestAssured.given()
-            .header("Authorization", "Bearer " + importer.getAccessToken())
-            .header("ServiceAuthorization", s2sToken)
-            .multiPart(new File("src/resource/CCD_CNP_27.xlsx"))
-            .expect()
-            .statusCode(201)
-            .when()
-            .post("/import");
     }
 
     private void createTestRole(AATHelper aat, String s2sToken) {
