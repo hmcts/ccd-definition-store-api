2edc980117e3e61ba2778f4095c5fd01c68c64a1
mutlu <mutlu.sancaktutar@hmcts.net>
2020-03-26 20:12:13 +0000

    cleanup

--- application/src/test/java/uk/gov/hmcts/net/ccd/definition/store/SwaggerPublisherTest.java
+++ application/src/test/java/uk/gov/hmcts/net/ccd/definition/store/SwaggerPublisherTest.java
@@ -4,28 +4,22 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-@TestPropertySource(properties = { "ccd.authorised.services=ccd_data" })
 public class SwaggerPublisherTest extends BaseTest {
 
     @Test
     @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
     public void generateDocs() throws Exception {
-        try {
             ResultActions perform = mockMvc.perform(get("/v2/api-docs"));
             byte[] specs = perform.andExpect(status().isOk()).andReturn().getResponse().getContentAsByteArray();
 
             try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/swagger-specs.json"))) {
                 outputStream.write(specs);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
