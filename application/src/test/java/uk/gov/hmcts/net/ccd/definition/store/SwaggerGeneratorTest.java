9f771cf3eb6ce1ecebe6fdbb9c143c5155628d57
mutlu <mutlu.sancaktutar@hmcts.net>
2020-04-22 14:09:57 +0100

    correct typo

--- application/src/test/java/uk/gov/hmcts/net/ccd/definition/store/SwaggerGeneratorTest.java
+++ application/src/test/java/uk/gov/hmcts/net/ccd/definition/store/SwaggerGeneratorTest.java
@@ -24,7 +24,7 @@ public class SwaggerGeneratorTest extends BaseTest {
                 .getResponse()
                 .getContentAsByteArray();
 
-        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/ccd-defination-store-api.json"))) {
+        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/ccd-definition-store-api.json"))) {
                 outputStream.write(specs);
         }
     }
