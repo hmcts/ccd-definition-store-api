0b17f0886c5f8540a65abbfe39aa025c78120de8
Alex McAusland <alex.mcausland@gmail.com>
2020-05-07 08:12:52 +0100

    tmp

--- application/src/main/java/uk/gov/hmcts/ccd/definition/store/ApplicationConfiguration.java
+++ application/src/main/java/uk/gov/hmcts/ccd/definition/store/ApplicationConfiguration.java
@@ -39,6 +39,11 @@ class ApplicationConfiguration {
         executor.setThreadNamePrefix(prefix);
         executor.setThreadNamePrefix(prefix);
         executor.setThreadNamePrefix(prefix);
+        executor.setThreadNamePrefix(prefix);
+        executor.setThreadNamePrefix(prefix);
+        executor.setThreadNamePrefix(prefix);
+        executor.setThreadNamePrefix(prefix);
+        executor.setThreadNamePrefix(prefix);
         executor.initialize();
         executor.initialize();
         return executor;
