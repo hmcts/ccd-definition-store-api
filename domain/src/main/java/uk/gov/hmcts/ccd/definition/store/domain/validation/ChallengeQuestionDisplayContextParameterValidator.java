3e0320ad934c1fb56499cf90ee0fea9853ca596e
it.consulting@tutanota.com <it.consulting@tutanota.com>
2020-09-18 12:32:21 +0100

    RDM-9031: Introduce NewChallengeQuestion Tab
           [RDM-9031] (https://tools.hmcts.net/jira/browse/RDM-9031).

--- domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/validation/ChallengeQuestionDisplayContextParameterValidator.java
+++ domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/validation/ChallengeQuestionDisplayContextParameterValidator.java
@@ -10,7 +10,6 @@ import uk.gov.hmcts.ccd.definition.store.repository.entity.ChallengeQuestionTabE
 import uk.gov.hmcts.ccd.definition.store.repository.entity.FieldTypeEntity;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -45,25 +44,5 @@ public class ChallengeQuestionDisplayContextParameterValidator extends AbstractD
     @Override
     protected String getSheetName(ChallengeQuestionTabEntity entity) {
         return CHALLENGE_QUESTION_TAB;
-    }
-
-    public boolean isAnOllowedType(String reference) {
-        return ALLOWED_FIELD_TYPES.contains(reference);
-    }
-
-    @Override
-    public ValidationResult validate(ChallengeQuestionTabEntity entity, List<ChallengeQuestionTabEntity> allGenericLayouts) {
-        final String displayContext = entity.getDisplayContextParameter();
-        final String questionReference = entity.getAnswerFieldType().getReference();
-        if (!isAnOllowedType(questionReference)) {
-            final ValidationResult validationResult = new ValidationResult();
-            if (displayContext != null) {
-                validationResult.addError(new SimpleValidationError(ERROR_MESSAGE + " value: " + displayContext +
-                    " is not a valid DisplayContextParameter value for type: " + questionReference, entity)
-                );
-            }
-            return validationResult;
-        }
-        return super.validate(entity, Collections.EMPTY_LIST);
     }
 }
