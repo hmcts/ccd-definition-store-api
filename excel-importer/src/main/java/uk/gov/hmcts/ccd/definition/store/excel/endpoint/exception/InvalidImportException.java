9a3c252857ca6108317ece5c03f9c8f29bdb2f7d
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-10-15 17:04:27 +0100

    RDM-9824: ChallengeQuestion tab issues fixed

--- excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/endpoint/exception/InvalidImportException.java
+++ excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/endpoint/exception/InvalidImportException.java
@@ -1,15 +1,11 @@
 package uk.gov.hmcts.ccd.definition.store.excel.endpoint.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * Exception thrown if import of a Case Definition sheet fails due to missing data.
  *
  * @author Daniel Lam (A533913)
  */
 public class InvalidImportException extends RuntimeException {
-    private static final Logger logger = LoggerFactory.getLogger(InvalidImportException.class);
 
     public InvalidImportException() {
         // default constructor
@@ -17,6 +13,5 @@ public class InvalidImportException extends RuntimeException {
 
     public InvalidImportException(final String message) {
         super(message);
-        logger.error(message);
     }
 }
