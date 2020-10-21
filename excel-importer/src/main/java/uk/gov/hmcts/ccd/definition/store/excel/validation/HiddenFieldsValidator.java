5e6fbc44d11c66cde014384be48114827313a097
RBaker <rebecca.baker@hmcts.net>
2020-10-21 10:28:27 +0100

    RDM-9830 updated logic

--- excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/validation/HiddenFieldsValidator.java
+++ excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/validation/HiddenFieldsValidator.java
@@ -50,8 +50,8 @@ public class HiddenFieldsValidator {
             }
         }
         if (definitionDataItem.getRetainHiddenValue() != null && !valid) {
-            throw new MapperException(String.format("'retainHiddenValue' has been incorrectly configured or is invalid for "
-                    + "fieldID ['%s'] on ['%s']",
+            throw new MapperException(String.format("'retainHiddenValue' has been incorrectly configured or "
+                    + "is invalid for fieldID ['%s'] on ['%s']",
                 caseFieldId, SheetName.CASE_EVENT_TO_FIELDS.getName()));
         }
     }
