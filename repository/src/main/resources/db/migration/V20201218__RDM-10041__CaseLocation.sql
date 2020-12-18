a1469936f18761bf76e9f7c1a8575d533720d662
Abi-Laver <abi.laver@cgi.com>
2020-12-18 11:16:27 +0000

    New Flyway naming convention & more readable field names

--- repository/src/main/resources/db/migration/V20201218__RDM-10041__CaseLocation.sql
+++ repository/src/main/resources/db/migration/V20201218__RDM-10041__CaseLocation.sql
@@ -9,11 +9,11 @@ values (now(), 'CaseLocation', 1,
 );
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('RegionId', 'RegionId', 'PUBLIC',
+values ('Region ID', 'Region ID', 'PUBLIC',
     (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
     (select id from field_type where reference = 'CaseLocation' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('BaseLocationId', 'BaseLocationId', 'PUBLIC',
+values ('Base Location ID', 'Base Location ID', 'PUBLIC',
     (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
     (select id from field_type where reference = 'CaseLocation' and version = 1 and jurisdiction_id is null));
