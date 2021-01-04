8fec0cda07147db580e66c049ec17d331edf432d
Abi-Laver <abi.laver@cgi.com>
2021-01-04 14:42:49 +0000

    Added ES mapping and altered SQL for fieldtypes

--- repository/src/main/resources/db/migration/V20201218__RDM-10041__CaseLocation.sql
+++ repository/src/main/resources/db/migration/V20201218__RDM-10041__CaseLocation.sql
@@ -7,32 +7,18 @@ values (now(), 'CaseLocation', 1,
         and jurisdiction_id is null
         and base_field_type_id is null))
 );
+INSERT INTO public.field_type (created_at, reference, version)
+VALUES (now(), 'Region', '1');
 
-insert into field_type (created_at, reference, version, base_field_type_id)
-values (now(), 'Region', 1,
-    (select id from field_type where reference = null
-        and jurisdiction_id is null
-        and version = (select max(version)
-        from field_type where reference = null
-        and jurisdiction_id is null
-        and base_field_type_id is null))
-);
-insert into field_type (created_at, reference, version, base_field_type_id)
-values (now(), 'BaseLocation', 1,
-    (select id from field_type where reference = null
-        and jurisdiction_id is null
-        and version = (select max(version)
-        from field_type where reference = null
-        and jurisdiction_id is null
-        and base_field_type_id is null))
-);
+INSERT INTO public.field_type (created_at, reference, version)
+VALUES (now(), 'BaseLocation', '1');
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('Region ID', 'Region ID', 'PUBLIC',
+values ('RegionId', 'Region ID', 'PUBLIC',
     (select id from field_type where reference = 'Region' and version = 1 and jurisdiction_id is null),
     (select id from field_type where reference = 'CaseLocation' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('Base Location ID', 'Base Location ID', 'PUBLIC',
+values ('BaseLocationId', 'Base Location ID', 'PUBLIC',
     (select id from field_type where reference = 'BaseLocation' and version = 1 and jurisdiction_id is null),
     (select id from field_type where reference = 'CaseLocation' and version = 1 and jurisdiction_id is null));
