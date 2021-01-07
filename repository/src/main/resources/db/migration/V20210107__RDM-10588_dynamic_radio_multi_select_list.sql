6b610ed9ec0ed061b06cafe9a7b46115883b1a9d
kiran.yenigala@hmcts.net <Yrlks1091327~>
2021-01-07 14:53:29 +0000

    RDM-10588: DynamicRadioList and DynamicMultiSelectList

---
+++ repository/src/main/resources/db/migration/V20210107__RDM-10588_dynamic_radio_multi_select_list.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.field_type (created_at, reference, version)
+VALUES (now(), 'DynamicRadioList', '1');
+
+INSERT INTO public.field_type (created_at, reference, version)
+VALUES (now(), 'DynamicMultiSelectList', '1');
