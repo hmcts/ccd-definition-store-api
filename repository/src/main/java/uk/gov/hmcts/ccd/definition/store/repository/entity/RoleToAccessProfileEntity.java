93e1a957777bce1187c40db2bb993d15cd707d66
kiran.yenigala@hmcts.net <Yrlks1091327~>
2021-02-04 09:22:22 +0000

    RDM-10538: Checkstyle issues fixed

--- repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/entity/RoleToAccessProfileEntity.java
+++ repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/entity/RoleToAccessProfileEntity.java
@@ -12,7 +12,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import static javax.persistence.FetchType.LAZY;
-import static javax.persistence.GenerationType.IDENTITY;
 
 @Table(name = "role_to_access_profile")
 @Entity
