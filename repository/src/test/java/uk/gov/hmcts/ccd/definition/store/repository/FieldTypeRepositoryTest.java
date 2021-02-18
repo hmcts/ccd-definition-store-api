a7530aa53a1e934d7a05156dbc2f9d5aae3230ab
Dan Lysiak <50049163+danlysiak@users.noreply.github.com>
2021-02-18 20:13:37 +0000

    Update perftest with latest release cut (#941)
    * RDM-10041 Create CaseLocation complex type (#876)
    
    * Create CaseLocation complex type
    
    * New Flyway naming convention & more readable field names
    
    * New fieldtypes BaseLocation & Region
    
    * Added ES mapping and altered SQL for fieldtypes
    
    * Addition of CVE-2020-26939 dependency suppression
    
    * More dependency suppression
    
    * Updated test to include new basetypes
    
    * Increased expected number of basetypes
    
    * Added new BaseTypes to functional test
    
    * Merge fixes
    
    * Update S-331.td.json
    
    Co-authored-by: Ashley Noronha <ashleydeannoronha@gmail.com>
    Co-authored-by: Hemanth <hrpotipati@gmail.com>
    
    * Rename V20201217__RDM-10161_previous_organisations.sql to V20201217_10161__RDM-10161_previous_organisations.sql
    
    * Correct test assertion following merge
    
    Co-authored-by: Abi-Laver <72914860+Abi-Laver@users.noreply.github.com>
    Co-authored-by: Ashley Noronha <ashleydeannoronha@gmail.com>
    Co-authored-by: Hemanth <hrpotipati@gmail.com>

--- repository/src/test/java/uk/gov/hmcts/ccd/definition/store/repository/FieldTypeRepositoryTest.java
+++ repository/src/test/java/uk/gov/hmcts/ccd/definition/store/repository/FieldTypeRepositoryTest.java
@@ -32,11 +32,14 @@ import static org.junit.Assert.assertThat;
 import static uk.gov.hmcts.ccd.definition.store.repository.FieldTypeUtils.PREDEFINED_COMPLEX_ADDRESS_GLOBAL;
 import static uk.gov.hmcts.ccd.definition.store.repository.FieldTypeUtils.PREDEFINED_COMPLEX_ADDRESS_GLOBAL_UK;
 import static uk.gov.hmcts.ccd.definition.store.repository.FieldTypeUtils.PREDEFINED_COMPLEX_ADDRESS_UK;
+import static uk.gov.hmcts.ccd.definition.store.repository.FieldTypeUtils.PREDEFINED_COMPLEX_CASELINK;
 import static uk.gov.hmcts.ccd.definition.store.repository.FieldTypeUtils.PREDEFINED_COMPLEX_CHANGE_ORGANISATION_REQUEST;
 import static uk.gov.hmcts.ccd.definition.store.repository.FieldTypeUtils.PREDEFINED_COMPLEX_ORDER_SUMMARY;
 import static uk.gov.hmcts.ccd.definition.store.repository.FieldTypeUtils.PREDEFINED_COMPLEX_ORGANISATION;
 import static uk.gov.hmcts.ccd.definition.store.repository.FieldTypeUtils.PREDEFINED_COMPLEX_ORGANISATION_POLICY;
 import static uk.gov.hmcts.ccd.definition.store.repository.FieldTypeUtils.PREDEFINED_COMPLEX_PREVIOUS_ORGANISATION;
+import static uk.gov.hmcts.ccd.definition.store.repository.FieldTypeUtils.PREDEFINED_COMPLEX_CASE_LOCATION;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {
@@ -223,17 +226,19 @@ public class FieldTypeRepositoryTest {
 
         List<FieldTypeEntity> predefinedComplexTypes = fieldTypeRepository.findPredefinedComplexTypes();
 
-        assertEquals(9, predefinedComplexTypes.size());
+        assertEquals(10, predefinedComplexTypes.size());
 
         assertThat(predefinedComplexTypes, hasItems(
             fieldTypeWithReference(PREDEFINED_COMPLEX_ADDRESS_GLOBAL),
             fieldTypeWithReference(PREDEFINED_COMPLEX_ADDRESS_UK),
             fieldTypeWithReference(PREDEFINED_COMPLEX_ADDRESS_GLOBAL_UK),
             fieldTypeWithReference(PREDEFINED_COMPLEX_ORDER_SUMMARY),
+            fieldTypeWithReference(PREDEFINED_COMPLEX_CASELINK),
             fieldTypeWithReference(PREDEFINED_COMPLEX_ORGANISATION),
             fieldTypeWithReference(PREDEFINED_COMPLEX_ORGANISATION_POLICY),
             fieldTypeWithReference(PREDEFINED_COMPLEX_CHANGE_ORGANISATION_REQUEST),
-            fieldTypeWithReference(PREDEFINED_COMPLEX_PREVIOUS_ORGANISATION)
+            fieldTypeWithReference(PREDEFINED_COMPLEX_PREVIOUS_ORGANISATION),
+            fieldTypeWithReference(PREDEFINED_COMPLEX_CASE_LOCATION)
             )
         );
     }
