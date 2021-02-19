2c300574edc8c91329ccbfa99868dd0cd00795d4
Abi-Laver <abi.laver@cgi.com>
2021-02-19 15:13:30 +0000

    Moved to new file

---
+++ repository/src/main/resources/db/migration/V20210219_11046__RDM-11046_prepopulate_to_users_organisation.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
+VALUES ('PrepopulateToUsersOrganisation', 'Prepopulate User Organisation', 'PUBLIC',
+        (select id from field_type where reference = 'YesOrNo' and version = 1 and jurisdiction_id is null),
+        (select id from field_type where reference = 'OrganisationPolicy' and version = 1 and jurisdiction_id is null));
