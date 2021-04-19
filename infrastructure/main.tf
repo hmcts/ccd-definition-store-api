07a4e7bc6b533d3d49adad9b0b92665ad8f0d660
nathan-clark <56871207+nathan-clark@users.noreply.github.com>
2021-04-19 15:02:24 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -43,6 +43,42 @@ data "azurerm_key_vault_secret" "definition_store_s2s_secret" {
   key_vault_id = "${data.azurerm_key_vault.s2s_vault.id}"
 }
 
+
+
+
+////////////////////////////////////////////////////////////
+// DB version OLD KEYS this values are going to be deleted//
+///////////////////////////////////////////////////////////
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+  name         = "${var.component}-POSTGRES-USER"
+  value        = module.definition-store-db-v11.user_name
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+  name         = "${var.component}-POSTGRES-PASS"
+  value        = module.definition-store-db-v11.postgresql_password
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+  name         = "${var.component}-POSTGRES-HOST"
+  value        = module.definition-store-db-v11.host_name
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+  name         = "${var.component}-POSTGRES-PORT"
+  value        = module.definition-store-db-v11.postgresql_listen_port
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+  name         = "${var.component}-POSTGRES-DATABASE"
+  value        = module.definition-store-db-v11.postgresql_database
+  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+}
+
 ////////////////////////////////
 // DB version 11             //
 ////////////////////////////////
@@ -63,36 +99,4 @@ module "definition-store-db-v11" {
   sku_capacity    = "${var.database_sku_capacity}"
   storage_mb      = "51200"
   common_tags     = "${var.common_tags}"
-}
-
-
-
-resource "azurerm_key_vault_secret" "POSTGRES-USER-V11" {
-  name         = "${var.component}-POSTGRES-USER-V11"
-  value        = module.definition-store-db-v11.user_name
-  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-PASS-V11" {
-  name         = "${var.component}-POSTGRES-PASS-V11"
-  value        = module.definition-store-db-v11.postgresql_password
-  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_HOST-V11" {
-  name         = "${var.component}-POSTGRES-HOST-V11"
-  value        = module.definition-store-db-v11.host_name
-  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_PORT-V11" {
-  name         = "${var.component}-POSTGRES-PORT-V11"
-  value        = module.definition-store-db-v11.postgresql_listen_port
-  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_DATABASE-V11" {
-  name         = "${var.component}-POSTGRES-DATABASE-V11"
-  value        = module.definition-store-db-v11.postgresql_database
-  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
 }
