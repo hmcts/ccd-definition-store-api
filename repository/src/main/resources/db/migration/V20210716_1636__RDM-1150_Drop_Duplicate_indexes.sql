07af5e11a1a7eb52bc8ea79a666f3acb8726d28a
OlanrewajuAdede <lanre.adedeji@hmcts.net>
2021-07-27 11:41:34 +0100

    And Updated Dropped indexes  sql file Update Daily CCD Suppressions,

---
+++ repository/src/main/resources/db/migration/V20210716_1636__RDM-1150_Drop_Duplicate_indexes.sql
@@ -0,0 +1,6 @@
+DROP INDEX idx_reference  CASCADE;
+DROP INDEX idx_event__case_type_id  CASCADE;
+DROP INDEX event_pre_state_state_id_event_id_idx  CASCADE;
+DROP INDEX idx_field_type__id__base_field_type_id__collection_field_type_i  CASCADE;
+DROP INDEX idx_jurisdiction__id__reference  CASCADE;
+DROP INDEX idx_field_type_list_item__field_type_id  CASCADE;
