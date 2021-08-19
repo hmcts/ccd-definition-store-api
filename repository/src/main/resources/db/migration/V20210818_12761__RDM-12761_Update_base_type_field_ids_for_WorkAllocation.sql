f08f14b1ceae9db092f9fa6fb37629e61e99ec91
padmapriyanalam <padmapriya.nalam@gmail.com>
2021-08-19 11:03:36 +0100

    RDM-12761 Updated the column names

--- repository/src/main/resources/db/migration/V20210818_12761__RDM-12761_Update_base_type_field_ids_for_WorkAllocation.sql
+++ repository/src/main/resources/db/migration/V20210818_12761__RDM-12761_Update_base_type_field_ids_for_WorkAllocation.sql
@@ -1,3 +1,3 @@
-UPDATE complex_field SET reference = 'region' WHERE reference='RegionId';
+UPDATE complex_field SET reference = 'region' WHERE reference='RegionId' and complex_field_type_id = (select id from field_type where reference = 'CaseLocation' and jurisdiction_id is null);
 
-UPDATE complex_field SET reference = 'baseLocation' WHERE reference='BaseLocationId';
+UPDATE complex_field SET reference = 'baseLocation' WHERE reference='BaseLocationId' and complex_field_type_id = (select id from field_type where reference = 'CaseLocation' and jurisdiction_id is null);
