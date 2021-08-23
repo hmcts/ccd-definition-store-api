499dbd2628ccc00e63f4d6c97cae9f27815d77a6
padmapriyanalam <padmapriya.nalam@gmail.com>
2021-08-23 10:28:33 +0100

    RDM-12761 Updated the label names

--- repository/src/main/resources/db/migration/V20210818_12761__RDM-12761_Update_base_type_field_ids_for_WorkAllocation.sql
+++ repository/src/main/resources/db/migration/V20210818_12761__RDM-12761_Update_base_type_field_ids_for_WorkAllocation.sql
@@ -1,3 +1,5 @@
-UPDATE complex_field SET reference = 'region' WHERE reference='RegionId' and complex_field_type_id = (select id from field_type where reference = 'CaseLocation' and jurisdiction_id is null);
+UPDATE complex_field SET reference = 'region' , label='Region' WHERE reference='RegionId'
+and complex_field_type_id = (select id from field_type where reference = 'CaseLocation' and jurisdiction_id is null);
 
-UPDATE complex_field SET reference = 'baseLocation' WHERE reference='BaseLocationId' and complex_field_type_id = (select id from field_type where reference = 'CaseLocation' and jurisdiction_id is null);
+UPDATE complex_field SET reference = 'baseLocation', label='Base Location' WHERE reference='BaseLocationId'
+and complex_field_type_id = (select id from field_type where reference = 'CaseLocation' and jurisdiction_id is null);
