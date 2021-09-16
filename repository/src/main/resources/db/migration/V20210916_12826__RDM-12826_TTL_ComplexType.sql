2a07360bad3909042aa7b43c9917c50c6a58d5d0
KleoG <kleogeorgiou@hotmail.com>
2021-09-16 16:15:44 +0100

    RDM-12826 - fix script bug

--- repository/src/main/resources/db/migration/V20210916_12826__RDM-12826_TTL_ComplexType.sql
+++ repository/src/main/resources/db/migration/V20210916_12826__RDM-12826_TTL_ComplexType.sql
@@ -11,7 +11,7 @@ values (now(), 'TTL', 1,
                             and base_field_type_id is null)));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('SystemTTL', 'System TTL', 'PUBLIC',c
+values ('SystemTTL', 'System TTL', 'PUBLIC',
         (select id from field_type where reference = 'Date' and version = 1 and jurisdiction_id is null),
         (select id from field_type where reference = 'TTL' and version = 1 and jurisdiction_id is null));
 
