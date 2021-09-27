1d7364fe907d66b9d689c5f3fce557aaf86d7e1a
RBaker <rebecca.baker@hmcts.net>
2021-09-27 16:27:22 +0100

    RDM-12812 added dateOfDeath to searchParty minor refactor

--- repository/src/main/resources/db/migration/V20210924_12812__RDM-12812-SearchParty_ComplexType_DOD.sql
+++ repository/src/main/resources/db/migration/V20210924_12812__RDM-12812-SearchParty_ComplexType_DOD.sql
@@ -1,4 +1,6 @@
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('DateOfDeath', 'DateOfDeath', 'PUBLIC',
-        (select id from field_type where reference = 'DateTime' and version = 1 and jurisdiction_id is null),
+        (select id from field_type where reference = 'Date' and version = 1 and jurisdiction_id is null),
         (select id from field_type where reference = 'SearchParty' and version = 1 and jurisdiction_id is null));
+
+ALTER TABLE search_party ADD COLUMN search_party_dod VARCHAR(2000);
