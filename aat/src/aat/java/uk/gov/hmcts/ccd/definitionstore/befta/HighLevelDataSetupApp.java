2e5643da9b106cfb26f085a3321ab1551596ac06
Mutlu Sancaktutar <mutlu.sancaktutar@hmcts.net>
2021-12-03 11:15:45 +0000

    temp disable HLDS

--- aat/src/aat/java/uk/gov/hmcts/ccd/definitionstore/befta/HighLevelDataSetupApp.java
+++ aat/src/aat/java/uk/gov/hmcts/ccd/definitionstore/befta/HighLevelDataSetupApp.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.ccd.definitionstore.befta;
 import uk.gov.hmcts.befta.dse.ccd.CcdEnvironment;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
 
+import java.util.Locale;
+
 public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
 
     public HighLevelDataSetupApp(CcdEnvironment dataSetupEnvironment) {
@@ -10,7 +12,9 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
     }
 
     public static void main(String[] args) throws Throwable {
-        //main(HighLevelDataSetupApp.class, args);
+        if(!args[0].toString().toLowerCase().equals("prod")) {
+            main(HighLevelDataSetupApp.class, args);
+        }
     }
 
 }
