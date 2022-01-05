2a17630feaa8824084445bf1576e32ae2757e0a4
kiran.yenigala@hmcts.net <Yrlks1091327~>
2022-01-05 09:40:00 +0000

    RDM-12979: address code review comments

--- repository/src/main/resources/db/migration/V20211220_12979__RDM-12979__FlagsFlagDetails.sql
+++ repository/src/main/resources/db/migration/V20211220_12979__RDM-12979__FlagsFlagDetails.sql
@@ -45,15 +45,11 @@ values ('dateTimeCreated', 'Created Date', 'PUBLIC',
 
 
 --- Path and Path Collection ---
-INSERT INTO public.field_type (created_at, reference, base_field_type_id, version)
-VALUES (now(), 'Path',
-(select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
-'1');
 
 INSERT INTO public.field_type (created_at, reference, base_field_type_id, collection_field_type_id, version)
 VALUES (now(), 'PathCollection',
 (select id from field_type where reference = 'Collection' and version = 1 and jurisdiction_id is null),
-(select id from field_type where reference = 'Path' and version = 1 and jurisdiction_id is null), '1');
+(select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null), '1');
 
 --- Add Path Collection to FlgDetails ---
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
@@ -101,17 +97,17 @@ and base_field_type_id is null))
 );
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('FlagType', 'Flag Type', 'PUBLIC',
+values ('roleOnCase', 'Flag Type', 'PUBLIC',
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'Flags' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('PartyName', 'Party Name', 'PUBLIC',
+values ('partyName', 'Party Name', 'PUBLIC',
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'Flags' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('FlagDetails', 'Flag Details', 'PUBLIC',
+values ('details', 'Flag Details', 'PUBLIC',
 (select id from field_type where reference = 'FlagDetailsCollection' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'Flags' and version = 1 and jurisdiction_id is null));
 
