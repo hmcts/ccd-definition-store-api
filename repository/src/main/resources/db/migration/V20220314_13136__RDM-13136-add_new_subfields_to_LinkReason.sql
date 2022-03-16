d6abb8451c9765671654a5e646b9d6a6cd493184
KleoG <kleogeorgiou@hotmail.com>
2022-03-16 11:35:51 +0000

    RDM-13136 - sql script consistency update

--- repository/src/main/resources/db/migration/V20220314_13136__RDM-13136-add_new_subfields_to_LinkReason.sql
+++ repository/src/main/resources/db/migration/V20220314_13136__RDM-13136-add_new_subfields_to_LinkReason.sql
@@ -3,7 +3,7 @@ VALUES (now(), 'ReasonForLinkList',
         (select id from field_type where reference = 'Collection' and version = 1 and jurisdiction_id is null),
         (select id from field_type where reference = 'LinkReason' and version = 1 and jurisdiction_id is null), '1');
 
-insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
+insert into public.complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('ReasonForLink', 'ReasonForLink', 'PUBLIC',
         (select id from field_type where reference = 'ReasonForLinkList' and version = 1 and jurisdiction_id is null),
         (select id from field_type where reference = 'CaseLink' and version = 1 and jurisdiction_id is null));
