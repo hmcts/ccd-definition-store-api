b6a3abddcc0f83082dc051b5a9ec32ff773ca492
padmapriyanalam <padmapriya.nalam@gmail.com>
2022-05-18 09:30:18 +0100

    WLTS-6 added host values

--- excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/Client/Translation/DictionaryRequest.java
+++ excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/Client/Translation/DictionaryRequest.java
@@ -1,12 +1,10 @@
 package uk.gov.hmcts.ccd.definition.store.excel.client.translation;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class DictionaryRequest {
