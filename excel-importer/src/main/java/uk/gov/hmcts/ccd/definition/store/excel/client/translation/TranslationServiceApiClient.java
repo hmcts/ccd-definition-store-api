1d31736e4dfbaa6cc69eb55e4613dea9489ebc18
padmapriyanalam <padmapriya.nalam@gmail.com>
2022-05-18 12:21:51 +0100

    WLTS-6 Sonarlint issues

--- excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/client/translation/TranslationServiceApiClient.java
+++ excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/client/translation/TranslationServiceApiClient.java
@@ -1,10 +1,8 @@
 package uk.gov.hmcts.ccd.definition.store.excel.client.translation;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
