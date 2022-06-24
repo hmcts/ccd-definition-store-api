cfc7ff60e6a3b743777e969e94bc4fa40288fd31
markdathorne <mark.dathorne@cgi.com>
2022-06-24 12:50:15 +0100

    CCD-3384 code reviewed SQL improvements

--- repository/src/main/resources/db/migration/V20220622_3384__CCD-3384_Update_retain_hidden_values_for_Flags_field_type.sql
+++ repository/src/main/resources/db/migration/V20220622_3384__CCD-3384_Update_retain_hidden_values_for_Flags_field_type.sql
@@ -1,2 +1,4 @@
 UPDATE complex_field SET retain_hidden_value = true
-where complex_field_type_id in (2301, 2451);
+where complex_field_type_id in (
+(select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null),
+(select id from field_type where reference = 'Flags' and version = 1 and jurisdiction_id is null));
