ec2cbc89583d86280e4238cd84ac7235c1bb4465
Mike Brown <michael.brown@hmcts.net>
2022-08-23 18:42:21 +0100

    CCD-3509 CVE-2021-22044 simple empty ArrayList returned in saveAllAndFlush

--- repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/VersionedDefinitionRepositoryDecorator.java
+++ repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/VersionedDefinitionRepositoryDecorator.java
@@ -5,11 +5,9 @@ import org.springframework.data.repository.query.FluentQuery;
 import uk.gov.hmcts.ccd.definition.store.repository.entity.Versionable;
 
 import java.io.Serializable;
-import java.util.Collection;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Iterator;
 import java.util.Optional;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.function.Function;
 
 @SuppressWarnings({"checkstyle:InterfaceTypeParameterName", "checkstyle:ClassTypeParameterName"})
@@ -38,122 +36,7 @@ public class VersionedDefinitionRepositoryDecorator<T extends Versionable, ID ex
 
     @Override
     public <S extends T> List<S> saveAllAndFlush(Iterable<S> entities) {
-        return new List<S>() {
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<S> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(S s) {
-                return false;
-            }
-
-            @Override
-            public void add(int index, S element) {
-
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public S remove(int index) {
-                return null;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends S> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int index, Collection<? extends S> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public S get(int index) {
-                return null;
-            }
-
-            @Override
-            public S set(int index, S element) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public ListIterator<S> listIterator() {
-                return null;
-            }
-
-            @Override
-            public ListIterator<S> listIterator(int index) {
-                return null;
-            }
-
-            @Override
-            public List<S> subList(int fromIndex, int toIndex) {
-                return null;
-            }
-        };
+        return new ArrayList<>();
     }
 
     @Override
