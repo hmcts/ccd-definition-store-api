89d4b673d10447ada59187eb85c224f71e27c198
Mike Brown <michael.brown@hmcts.net>
2022-08-23 17:16:09 +0100

    CCD-3509 CVE-2021-22044 newline added to VersionedDefinitionRepositoryDecoratorTest for checkstyle

--- repository/src/test/java/uk/gov/hmcts/ccd/definition/store/repository/VersionedDefinitionRepositoryDecoratorTest.java
+++ repository/src/test/java/uk/gov/hmcts/ccd/definition/store/repository/VersionedDefinitionRepositoryDecoratorTest.java
@@ -152,6 +152,7 @@ public class VersionedDefinitionRepositoryDecoratorTest {
             }
         });
     }
+
     @Test
     public void getByIdTest() {
         versionedJurisdictionRepository.getById(Integer.MAX_VALUE);
