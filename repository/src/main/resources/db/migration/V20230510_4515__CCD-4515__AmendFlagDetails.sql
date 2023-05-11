d5d0ad1650b6bb04cdb5ec7bcc1b9565866a7149
marvincudjoe <18750678+marvincudjoe@users.noreply.github.com>
2023-05-11 12:41:08 +0100

    Set retain_hidden_value to true

--- repository/src/main/resources/db/migration/V20230510_4515__CCD-4515__AmendFlagDetails.sql
+++ repository/src/main/resources/db/migration/V20230510_4515__CCD-4515__AmendFlagDetails.sql
@@ -9,12 +9,12 @@ values ('subTypeValue_cy', 'Flag subType Value In Welsh', 'PUBLIC', true,
 (select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, retain_hidden_value, field_type_id, complex_field_type_id)
-values ('otherDescription_cy', 'Flag Name In Welsh', 'PUBLIC', true
+values ('otherDescription_cy', 'Flag Name In Welsh', 'PUBLIC', true,
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, retain_hidden_value, field_type_id, complex_field_type_id)
-values ('flagComment_cy', 'Additional Comments In Welsh', 'PUBLIC', true
+values ('flagComment_cy', 'Additional Comments In Welsh', 'PUBLIC', true,
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
 
