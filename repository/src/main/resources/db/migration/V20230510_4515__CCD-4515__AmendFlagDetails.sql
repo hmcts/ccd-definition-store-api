9e26f5232a4e52dd84a13c0fae142ee63d9793a1
marvincudjoe <18750678+marvincudjoe@users.noreply.github.com>
2023-05-11 12:30:19 +0100

    Set retain_hidden_value to true

--- repository/src/main/resources/db/migration/V20230510_4515__CCD-4515__AmendFlagDetails.sql
+++ repository/src/main/resources/db/migration/V20230510_4515__CCD-4515__AmendFlagDetails.sql
@@ -1,29 +1,29 @@
-insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('name_cy', 'Name of Flag In Welsh', 'PUBLIC',
+insert into complex_field (reference, label, security_classification, retain_hidden_value, field_type_id, complex_field_type_id)
+values ('name_cy', 'Name of Flag In Welsh', 'PUBLIC', true,
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
 
-insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('subTypeValue_cy', 'Flag subType Value In Welsh', 'PUBLIC',
+insert into complex_field (reference, label, security_classification, retain_hidden_value, field_type_id, complex_field_type_id)
+values ('subTypeValue_cy', 'Flag subType Value In Welsh', 'PUBLIC', true,
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
 
-insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('otherDescription_cy', 'Flag Name In Welsh', 'PUBLIC',
+insert into complex_field (reference, label, security_classification, retain_hidden_value, field_type_id, complex_field_type_id)
+values ('otherDescription_cy', 'Flag Name In Welsh', 'PUBLIC', true
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
 
-insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('flagComment_cy', 'Additional Comments In Welsh', 'PUBLIC',
+insert into complex_field (reference, label, security_classification, retain_hidden_value, field_type_id, complex_field_type_id)
+values ('flagComment_cy', 'Additional Comments In Welsh', 'PUBLIC', true
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
 
-insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('flagUpdateComment', 'Flag Update Comment', 'PUBLIC',
+insert into complex_field (reference, label, security_classification, retain_hidden_value, field_type_id, complex_field_type_id)
+values ('flagUpdateComment', 'Flag Update Comment', 'PUBLIC', true,
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
 
-insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('availableExternally', 'Availability to Non Staff', 'PUBLIC',
+insert into complex_field (reference, label, security_classification, retain_hidden_value, field_type_id, complex_field_type_id)
+values ('availableExternally', 'Availability to Non Staff', 'PUBLIC', true,
 (select id from field_type where reference = 'YesOrNo' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
