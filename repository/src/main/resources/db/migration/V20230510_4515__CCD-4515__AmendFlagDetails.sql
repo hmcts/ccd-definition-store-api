50b0c142c2241ad6e328c5ebbf9113ff6be91977
marvincudjoe <18750678+marvincudjoe@users.noreply.github.com>
2023-05-16 12:18:41 +0100

    Amend label descriptions

--- repository/src/main/resources/db/migration/V20230510_4515__CCD-4515__AmendFlagDetails.sql
+++ repository/src/main/resources/db/migration/V20230510_4515__CCD-4515__AmendFlagDetails.sql
@@ -1,5 +1,10 @@
 insert into complex_field (reference, label, security_classification, retain_hidden_value, field_type_id, complex_field_type_id)
-values ('name_cy', 'Name of Flag In Welsh', 'PUBLIC', true,
+values ('flagUpdateComment', 'Update Comments', 'PUBLIC', true,
+(select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
+(select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
+
+insert into complex_field (reference, label, security_classification, retain_hidden_value, field_type_id, complex_field_type_id)
+values ('name_cy', 'Name in Welsh', 'PUBLIC', true,
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
 
@@ -15,11 +20,6 @@ values ('otherDescription_cy', 'Other Description in Welsh', 'PUBLIC', true,
 
 insert into complex_field (reference, label, security_classification, retain_hidden_value, field_type_id, complex_field_type_id)
 values ('flagComment_cy', 'Comments in Welsh', 'PUBLIC', true,
-(select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
-(select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
-
-insert into complex_field (reference, label, security_classification, retain_hidden_value, field_type_id, complex_field_type_id)
-values ('flagUpdateComment', 'Update Comments', 'PUBLIC', true,
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'FlagDetails' and version = 1 and jurisdiction_id is null));
 
