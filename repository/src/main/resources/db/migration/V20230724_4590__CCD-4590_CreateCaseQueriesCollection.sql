6c453fcd9ace656cd9ce9ca16347f0602ddd5fd8
kerry-annemclaughlin <@Elvis2001>
2023-07-24 22:49:30 +0100

    CCD-4412: removing features for now

--- repository/src/main/resources/db/migration/V20230724_4590__CCD-4590_CreateCaseQueriesCollection.sql
+++ repository/src/main/resources/db/migration/V20230724_4590__CCD-4590_CreateCaseQueriesCollection.sql
@@ -9,7 +9,7 @@ and base_field_type_id is null))
 );
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('idamId', 'IdamId', 'PUBLIC',
+values ('caseQueriesCollectionId', 'caseQueriesCollectionId', 'PUBLIC',
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'JudicialUser' and version = 1 and jurisdiction_id is null));
 
