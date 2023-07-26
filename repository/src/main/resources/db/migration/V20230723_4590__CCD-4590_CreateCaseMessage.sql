aeab410c585e76d6a143931b3ca6b61224d1e348
kerry-annemclaughlin <@Elvis2001>
2023-07-26 07:12:11 +0100

    CCD-4412: removing features for now

--- repository/src/main/resources/db/migration/V20230723_4590__CCD-4590_CreateCaseMessage.sql
+++ repository/src/main/resources/db/migration/V20230723_4590__CCD-4590_CreateCaseMessage.sql
@@ -11,49 +11,49 @@ and base_field_type_id is null))
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('caseMessageId', 'caseMessageId', 'PUBLIC',
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
-(select id from field_type where reference = 'JudicialUser' and version = 1 and jurisdiction_id is null));
+(select id from field_type where reference = 'CaseMessage' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('subject', 'subject', 'PUBLIC',
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
-(select id from field_type where reference = 'JudicialUser' and version = 1 and jurisdiction_id is null));
+(select id from field_type where reference = 'CaseMessage' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('body', 'body', 'PUBLIC',
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
-(select id from field_type where reference = 'JudicialUser' and version = 1 and jurisdiction_id is null));
+(select id from field_type where reference = 'CaseMessage' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('attachments', 'attachements', 'PUBLIC',
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
-(select id from field_type where reference = 'JudicialUser' and version = 1 and jurisdiction_id is null));
+(select id from field_type where reference = 'CaseMessage' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('isHearingRelated', 'isHearingRelated', 'PUBLIC',
 (select id from field_type where reference = 'YesOrNo' and version = 1 and jurisdiction_id is null),
-(select id from field_type where reference = 'JudicialUser' and version = 1 and jurisdiction_id is null));
+(select id from field_type where reference = 'CaseMessage' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('hearingDate', 'hearingDate', 'PUBLIC',
 (select id from field_type where reference = 'Date' and version = 1 and jurisdiction_id is null),
-(select id from field_type where reference = 'JudicialUser' and version = 1 and jurisdiction_id is null));
+(select id from field_type where reference = 'CaseMessage' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('createdOn', 'createdOn', 'PUBLIC',
 (select id from field_type where reference = 'Date' and version = 1 and jurisdiction_id is null),
-(select id from field_type where reference = 'JudicialUser' and version = 1 and jurisdiction_id is null));
+(select id from field_type where reference = 'CaseMessage' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('createdBy', 'createdBy', 'PUBLIC',
 (select id from field_type where reference = 'DateTime' and version = 1 and jurisdiction_id is null),
-(select id from field_type where reference = 'JudicialUser' and version = 1 and jurisdiction_id is null));
+(select id from field_type where reference = 'CaseMessage' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('parentId', 'parentId', 'PUBLIC',
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
-(select id from field_type where reference = 'JudicialUser' and version = 1 and jurisdiction_id is null));
+(select id from field_type where reference = 'CaseMessage' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('name', 'name', 'PUBLIC',
 (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
-(select id from field_type where reference = 'JudicialUser' and version = 1 and jurisdiction_id is null));
+(select id from field_type where reference = 'CaseMessage' and version = 1 and jurisdiction_id is null));
