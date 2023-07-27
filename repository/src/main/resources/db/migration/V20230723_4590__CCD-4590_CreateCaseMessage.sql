3b65b115949cb8611696670a331d992bbd6c322b
kerry-annemclaughlin <@Elvis2001>
2023-07-27 09:45:02 +0100

    CCD-4412: PR comments

--- repository/src/main/resources/db/migration/V20230723_4590__CCD-4590_CreateCaseMessage.sql
+++ repository/src/main/resources/db/migration/V20230723_4590__CCD-4590_CreateCaseMessage.sql
@@ -58,12 +58,12 @@ values ('hearingDate', 'hearingDate', 'PUBLIC',
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('createdOn', 'createdOn', 'PUBLIC',
-(select id from field_type where reference = 'Date' and version = 1 and jurisdiction_id is null),
+(select id from field_type where reference = 'DateTime' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'CaseMessage' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
 values ('createdBy', 'createdBy', 'PUBLIC',
-(select id from field_type where reference = 'DateTime' and version = 1 and jurisdiction_id is null),
+(select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
 (select id from field_type where reference = 'CaseMessage' and version = 1 and jurisdiction_id is null));
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
--- repository/src/main/resources/db/migration/V20230724_4590__CCD-4590_CreateCaseQueriesCollection.sql
+++ repository/src/main/resources/db/migration/V20230724_4590__CCD-4590_CreateCaseQueriesCollection.sql
@@ -22,6 +22,6 @@ values (now(), 'caseMessageCollection', 1,
 );
 
 insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('CaseMessages', 'Case messages', 'PUBLIC',
+values ('caseMessages', 'Case messages', 'PUBLIC',
     (select id from field_type where reference = 'caseMessageCollection' and version = 1 and jurisdiction_id is null),
     (select id from field_type where reference = 'CaseQueriesCollection' and version = 1 and jurisdiction_id is null));
