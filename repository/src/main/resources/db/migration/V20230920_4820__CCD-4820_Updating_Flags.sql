e066e9400dd107f7b9ba6264d884ce101f2041b6
kerry-annemclaughlin <@Elvis2001>
2023-09-25 12:18:38 +0100

    CCD-4820: adding extra fields to flags base type.

--- repository/src/main/resources/db/migration/V20230920_4820__CCD-4820_Updating_Flags.sql
+++ repository/src/main/resources/db/migration/V20230920_4820__CCD-4820_Updating_Flags.sql
@@ -7,5 +7,3 @@ INSERT INTO public.complex_field (reference, label, security_classification, fie
 VALUES ('visibility', '	Visibility should be one of 2 values: Internal or External', 'PUBLIC',
         (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
         (select id from field_type where reference = 'Flags' and version = 1 and jurisdiction_id is null));
-
-
