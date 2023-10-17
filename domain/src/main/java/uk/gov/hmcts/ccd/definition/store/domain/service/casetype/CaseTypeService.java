287340f2882506e83b9bf0dac4987b34f0b1033e
Jason Paige <j@jasonpaige.co.uk>
2023-10-17 14:49:46 +0100

    not sure thoser exclusions are working...

--- domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/service/casetype/CaseTypeService.java
+++ domain/src/main/java/uk/gov/hmcts/ccd/definition/store/domain/service/casetype/CaseTypeService.java
@@ -21,8 +21,6 @@ public interface CaseTypeService {
 
     Optional<CaseType> findByCaseTypeId(String id);
 
-    List<CaseType> findAllByCaseTypeIds(List<String> caseTypeIds);
-
     Optional<CaseTypeVersionInformation> findVersionInfoByCaseTypeId(String id);
 
     String findDefinitiveCaseTypeId(String id);
