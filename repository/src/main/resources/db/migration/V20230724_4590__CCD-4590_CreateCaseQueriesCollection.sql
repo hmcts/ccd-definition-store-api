c0868a6fa8af14f6b798cba96e4746e9eef65793
kerry-annemclaughlin <@Elvis2001>
2023-10-18 09:20:10 +0100

    CCD-4942: adding missing fields to case query collections.

--- repository/src/main/resources/db/migration/V20230724_4590__CCD-4590_CreateCaseQueriesCollection.sql
+++ repository/src/main/resources/db/migration/V20230724_4590__CCD-4590_CreateCaseQueriesCollection.sql
@@ -25,13 +25,3 @@ insert into complex_field (reference, label, security_classification, field_type
 values ('caseMessages', 'Case messages', 'PUBLIC',
     (select id from field_type where reference = 'caseMessageCollection' and version = 1 and jurisdiction_id is null),
     (select id from field_type where reference = 'CaseQueriesCollection' and version = 1 and jurisdiction_id is null));
-
-insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('partyName', 'party name', 'PUBLIC',
-    (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
-    (select id from field_type where reference = 'CaseQueriesCollection' and version = 1 and jurisdiction_id is null));
-
-insert into complex_field (reference, label, security_classification, field_type_id, complex_field_type_id)
-values ('roleOnCase', 'role on case', 'PUBLIC',
-    (select id from field_type where reference = 'Text' and version = 1 and jurisdiction_id is null),
-    (select id from field_type where reference = 'CaseQueriesCollection' and version = 1 and jurisdiction_id is null));
