3436fbe0412dac3bdad11907663b4063ee2c0270
dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>
2023-10-20 09:11:33 +0100

    CCD-4866 : appinsight config update (#1405)
    * appinsight config update
    
    * appinsight config
    
    * updated sonar exclusions
    
    * added package component scanning annotation
    
    * tidy up
    
    * updated to skip code coverage
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    * Update build.gradle
    
    excluded AppInsightsConfiguration
    
    * removed added config which is no longer required
    
    * fix code coverage stats
    
    * attempt to fix codecov
    
    * updated app-insights/build.gradle
    
    ---------
    
    Co-authored-by: RebeccaBaker <38425793+RebeccaBaker@users.noreply.github.com>

--- application/src/main/java/uk/gov/hmcts/ccd/definition/store/CaseDataAPIApplication.java
+++ application/src/main/java/uk/gov/hmcts/ccd/definition/store/CaseDataAPIApplication.java
@@ -3,9 +3,11 @@ package uk.gov.hmcts.ccd.definition.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@ComponentScan({ "uk.gov.hmcts.ccd" })
 @SuppressWarnings("HideUtilityClassConstructor")
 @EnableFeignClients
 @EnableAsync
