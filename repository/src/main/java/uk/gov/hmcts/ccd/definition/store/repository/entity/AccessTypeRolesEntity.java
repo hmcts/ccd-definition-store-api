313bd1d46a600322cc054a56ec76f50bcad42c43
Helen Bird <helen.bird@HMCTS.NET>
2023-11-22 14:26:06 +0000

    GA-18: undo compressed imports

--- repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/entity/AccessTypeRolesEntity.java
+++ repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/entity/AccessTypeRolesEntity.java
@@ -1,6 +1,12 @@
 package uk.gov.hmcts.ccd.definition.store.repository.entity;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
