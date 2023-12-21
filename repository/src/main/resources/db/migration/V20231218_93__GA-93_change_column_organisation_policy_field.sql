276de316d30c9f3452eebef19d2091416c9cd52c
Helen Bird <helen.bird@HMCTS.NET>
2023-12-21 09:46:40 +0000

    GA-93: change case_assigned_role_field column length

--- repository/src/main/resources/db/migration/V20231218_93__GA-93_change_column_organisation_policy_field.sql
+++ repository/src/main/resources/db/migration/V20231218_93__GA-93_change_column_organisation_policy_field.sql
@@ -1,2 +1,5 @@
 ALTER TABLE public.access_type_roles
     RENAME COLUMN organisation_policy_field TO case_assigned_role_field;
+
+ALTER TABLE public.access_type_roles
+    ALTER COLUMN case_assigned_role_field TYPE character varying(70);
