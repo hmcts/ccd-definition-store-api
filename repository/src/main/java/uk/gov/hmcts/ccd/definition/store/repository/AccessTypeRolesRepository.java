3545c50d43b7596f70022b4ed3b01ae91a368674
patelila <ila.patel@hmcts.net>
2024-01-20 12:45:44 +0000

    Query changed to get the right version of the casetypeId information and to ensure that not multiple rows of data are retrieved.

--- repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/AccessTypeRolesRepository.java
+++ repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/AccessTypeRolesRepository.java
@@ -8,15 +8,19 @@ import java.util.List;
 
 public interface AccessTypeRolesRepository extends JpaRepository<AccessTypeRolesEntity, Integer> {
 
-    @Query("select atr from AccessTypeRolesEntity atr where"
-        + " atr.caseTypeId.version = (select max(ct.version) from CaseTypeEntity ct)"
-        + " and atr.reference = ct1.reference")
+    @Query("select atr from AccessTypeRolesEntity atr, CaseTypeEntity ct where"
+        + " atr.caseTypeId.version "
+        + " = (select max(ct1.version) from CaseTypeEntity ct1, AccessTypeRolesEntity atr1 where"
+        + " ct1.id=atr1.caseTypeId)"
+        + " and atr.caseTypeId=ct.id")
     List<AccessTypeRolesEntity> findAllWithCaseTypeIds();
 
-    @Query("select atr from AccessTypeRolesEntity atr, CaseTypeEntity ct1 where atr.organisationProfileId"
-        + " in :organisationProfileIds"
-        + " and atr.caseTypeId.version = (select max(ct.version) from CaseTypeEntity ct)"
-        + " and atr.reference = ct1.reference")
+    @Query("select atr from AccessTypeRolesEntity atr, CaseTypeEntity ct where "
+        + " atr.organisationProfileId in :organisationProfileIds"
+        + " and atr.caseTypeId.version "
+        + " = (select max(ct1.version) from CaseTypeEntity ct1, AccessTypeRolesEntity atr1 where"
+        + " ct1.id=atr1.caseTypeId)"
+        + " and atr.caseTypeId=ct.id")
     List<AccessTypeRolesEntity> findByOrganisationProfileIds(List<String>  organisationProfileIds);
 
 }
