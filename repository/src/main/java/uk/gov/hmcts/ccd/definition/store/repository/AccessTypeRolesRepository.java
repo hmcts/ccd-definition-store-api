11cc864da54f497298e2793315d5303761c1e430
patelila <ila.patel@hmcts.net>
2024-01-26 11:30:22 +0000

    Fix for multiple accessTypes having casetype with different jurisdictions

--- repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/AccessTypeRolesRepository.java
+++ repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/AccessTypeRolesRepository.java
@@ -8,19 +8,22 @@ import java.util.List;
 
 public interface AccessTypeRolesRepository extends JpaRepository<AccessTypeRolesEntity, Integer> {
 
-    @Query("select atr from AccessTypeRolesEntity atr, CaseTypeEntity ct where"
-        + " atr.caseTypeId.version "
-        + " = (select max(ct1.version) from CaseTypeEntity ct1, AccessTypeRolesEntity atr1 where"
-        + " ct1.id=atr1.caseTypeId)"
-        + " and atr.caseTypeId=ct.id")
+    @Query("select atr from AccessTypeRolesEntity atr, CaseTypeEntity ct, JurisdictionEntity j where"
+        + " atr.caseTypeId.version = (select max(ct1.version) "
+        + " from CaseTypeEntity ct1, AccessTypeRolesEntity atr1 where "
+        + " atr1.caseTypeId = ct1.id and ct1.jurisdiction = j.id)"
+        + " and atr.caseTypeId=ct.id"
+        + " and ct.jurisdiction = j.id")
     List<AccessTypeRolesEntity> findAllWithCaseTypeIds();
 
-    @Query("select atr from AccessTypeRolesEntity atr, CaseTypeEntity ct where "
-        + " atr.organisationProfileId in :organisationProfileIds"
-        + " and atr.caseTypeId.version "
-        + " = (select max(ct1.version) from CaseTypeEntity ct1, AccessTypeRolesEntity atr1 where"
-        + " ct1.id=atr1.caseTypeId)"
-        + " and atr.caseTypeId=ct.id")
+    @Query("select atr from AccessTypeRolesEntity atr, CaseTypeEntity ct, JurisdictionEntity j"
+          + " where atr.organisationProfileId in :organisationProfileIds"
+          + " and atr.caseTypeId.version = (select max(ct1.version) "
+          + " from CaseTypeEntity ct1, AccessTypeRolesEntity atr1 where "
+          + " atr1.caseTypeId = ct1.id and ct1.jurisdiction = j.id)"
+          + " and atr.caseTypeId=ct.id"
+          + " and ct.jurisdiction = j.id")
     List<AccessTypeRolesEntity> findByOrganisationProfileIds(List<String>  organisationProfileIds);
+
 
 }
