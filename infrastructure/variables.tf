f411efed2e2c538309befb281b44b455f5e3e90c
Muhammad <muhammad.umerji@hmcts.net>
2024-04-18 16:21:18 +0100

    Adding variable tenent id

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -84,3 +84,6 @@ variable "kv_subscription" {
   description = "Update this with the name of the subscription where the single server key vault is. Defaults to DCD-CNP-DEV."
 
 }
+variable "tenant_id" {
+  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environment variables and not normally required to be specified."
+}
