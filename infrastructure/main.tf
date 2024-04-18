f49d4a4419385b816790bb635e13a4a430e7dc68
Muhammad <muhammad.umerji@hmcts.net>
2024-04-18 15:59:07 +0100

    Updating key-vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,6 +16,18 @@ locals {
 
 }
 
+module "key-vault" {
+  source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  product                 = var.product
+  env                     = var.env
+  tenant_id               = var.tenant_id
+  object_id               = var.jenkins_AAD_objectId
+  resource_group_name     = azurerm_resource_group.rg.name
+  product_group_name      = "dcd_ccd"
+  common_tags             = var.common_tags
+  create_managed_identity = true
+}
+
 data "azurerm_key_vault" "ccd_shared_key_vault" {
   name                = "${local.vaultName}"
   resource_group_name = "${local.sharedResourceGroup}"
