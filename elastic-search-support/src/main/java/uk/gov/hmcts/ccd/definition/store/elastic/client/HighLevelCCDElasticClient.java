be1e6cc794f117b1f07aafc03a7007a68ab267d0
patelila <ila.patel@hmcts.net>
2024-08-05 11:19:31 +0100

    Try out setting writeindex from null to false.

--- elastic-search-support/src/main/java/uk/gov/hmcts/ccd/definition/store/elastic/client/HighLevelCCDElasticClient.java
+++ elastic-search-support/src/main/java/uk/gov/hmcts/ccd/definition/store/elastic/client/HighLevelCCDElasticClient.java
@@ -43,7 +43,7 @@ public class HighLevelCCDElasticClient implements CCDElasticClient {
     public boolean createIndex(String indexName, String alias) throws IOException {
         log.info("creating index {} with alias {}", indexName, alias);
         CreateIndexRequest request = new CreateIndexRequest(indexName);
-        request.alias(new Alias(alias));
+        request.alias(new Alias(alias).writeIndex(false));
         String file = (alias.equalsIgnoreCase(GLOBAL_SEARCH))
             ? GLOBAL_SEARCH_CASES_INDEX_SETTINGS_JSON : CASES_INDEX_SETTINGS_JSON;
         request.settings(casesIndexSettings(file));
