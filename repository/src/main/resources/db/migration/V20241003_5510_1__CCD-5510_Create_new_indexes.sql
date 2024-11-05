366a1ac2a21b5675aaff66803489c5bbc7ab6489
AK <116068970+aktaskaan@users.noreply.github.com>
2024-11-05 12:35:50 +0000

    Update V20241003_5510_1__CCD-5510_Create_new_indexes.sql

--- repository/src/main/resources/db/migration/V20241003_5510_1__CCD-5510_Create_new_indexes.sql
+++ repository/src/main/resources/db/migration/V20241003_5510_1__CCD-5510_Create_new_indexes.sql
@@ -4,8 +4,8 @@ CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_complex_field_acl_case_field_id ON p
 
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_event_acl_event_id ON public.event_acl USING btree (event_id);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_state_acl_state_id_id ON state_acl USING btree (state_id, id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_state_acl_state_id_id ON public.state_acl USING btree (state_id, id);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_search_party_case_type_id ON search_party USING btree (case_type_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_search_party_case_type_id ON public.search_party USING btree (case_type_id);
 
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_case_type_lower_reference_version_desc ON case_type USING btree (LOWER(reference), version DESC);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_case_type_lower_reference_version_desc ON public.case_type USING btree (LOWER(reference), version DESC);
