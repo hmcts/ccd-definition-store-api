a7ca1a0e8006dd3ce69831d7de23bd79a989cecb
Sammi Chong <shinyee.chong@hmcts.net>
2025-03-04 11:55:20 +0000

    Set defaults

--- excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/endpoint/ImportController.java
+++ excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/endpoint/ImportController.java
@@ -34,7 +34,7 @@ public class ImportController {
 
     @RequestMapping(value = URI_IMPORT, method = RequestMethod.POST)
     public ResponseEntity processUpload(@RequestParam("file") MultipartFile file,
-                                        @RequestParam(value = "reindex", required = false, defaultValue = "true")
+                                        @RequestParam(value = "reindex", required = false, defaultValue = "false")
                                         boolean reindex,
                                         @RequestParam(value = "deleteOldIndex", required = false, defaultValue = "true")
                                         boolean deleteOldIndex) throws IOException {
