5bdeacb18aa6e4885cff7110ce691ffd3bbd5dd8
tanisha <tanisha.bhogal@hmcts.net>
2025-03-13 13:16:49 +0000

    add responses to swagger documentation

--- excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/endpoint/ImportController.java
+++ excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/endpoint/ImportController.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -41,7 +42,17 @@ public class ImportController {
         @ApiResponse(code = 401, message = "Unauthorized"),
         @ApiResponse(code = 403, message = "Forbidden"),
         @ApiResponse(code = 404, message = "Not Found"),
-        @ApiResponse(code = 422, message = "Unprocessable Entity")
+        @ApiResponse(code = 422, message = "Unprocessable Entity"),
+        @ApiResponse(code = 400, message = "A definition must contain exactly one Jurisdiction \t\n "
+            + "A definition must contain at least one Case Type \t\n "
+            + "A definition must contain a Case Field worksheet \t\n "
+            + "A definition must contain a Complex Types worksheet \t\n "
+            + "A definition must contain a Fixed List worksheet \t\n "
+            + "Display context parameter {} has been incorrectly configured or is "
+            + "invalid for field {} on tab {} \t\n "
+            + "Error processing sheet {} Invalid Case Definition sheet - no Definition "
+            + "name found in Cell A1"),
+        @ApiResponse(code = 500, message = "Internal Server Error")
     })
 
     public ResponseEntity processUpload(@RequestParam("file") MultipartFile file) throws IOException {
