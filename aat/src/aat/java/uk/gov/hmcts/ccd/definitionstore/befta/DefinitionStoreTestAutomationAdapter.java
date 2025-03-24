8e5e7ab47e795a8b6c589048a4f5f940b53fafed
Dinesh Patel <dinesh1.patel@btinternet.com>
2025-03-24 16:14:30 +0000

    added swagger tests

--- aat/src/aat/java/uk/gov/hmcts/ccd/definitionstore/befta/DefinitionStoreTestAutomationAdapter.java
+++ aat/src/aat/java/uk/gov/hmcts/ccd/definitionstore/befta/DefinitionStoreTestAutomationAdapter.java
@@ -3,8 +3,10 @@ package uk.gov.hmcts.ccd.definitionstore.befta;
 import uk.gov.hmcts.befta.BeftaTestDataLoader;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
+import uk.gov.hmcts.befta.exception.FunctionalTestException;
 import uk.gov.hmcts.befta.player.BackEndFunctionalTestScenarioContext;
 import uk.gov.hmcts.befta.util.BeftaUtils;
+import uk.gov.hmcts.befta.util.ReflectionUtils;
 
 import static uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore.VALID_CCD_TEST_DEFINITIONS_PATH;
 
@@ -24,6 +26,35 @@ public class DefinitionStoreTestAutomationAdapter extends DefaultTestAutomationA
     public synchronized Object calculateCustomValue(BackEndFunctionalTestScenarioContext scenarioContext, Object key) {
         if (key.toString().startsWith("no_dynamic_injection_")) {
             return key.toString().replace("no_dynamic_injection_","");
+        } else if (key.toString().startsWith("approximately ")) {
+            try {
+                String actualSizeFromHeaderStr = (String) ReflectionUtils.deepGetFieldInObject(scenarioContext,
+                        "testData.actualResponse.headers.Content-Length");
+                String expectedSizeStr = key.toString().replace("approximately ", "");
+
+                int actualSize =  Integer.parseInt(actualSizeFromHeaderStr);
+                int expectedSize = Integer.parseInt(expectedSizeStr);
+
+                if (Math.abs(actualSize - expectedSize) < (actualSize * 10 / 100)) {
+                    return actualSizeFromHeaderStr;
+                }
+                return expectedSize;
+            } catch (Exception e) {
+                throw new FunctionalTestException("Problem checking acceptable response payload: ", e);
+            }
+        } else if (key.toString().startsWith("contains ")) {
+            try {
+                String actualValueStr = (String) ReflectionUtils.deepGetFieldInObject(scenarioContext,
+                    "testData.actualResponse.body.__plainTextValue__");
+                String expectedValueStr = key.toString().replace("contains ", "");
+
+                if (actualValueStr.contains(expectedValueStr)) {
+                    return actualValueStr;
+                }
+                return "expectedValueStr " + expectedValueStr + " not present in response ";
+            } catch (Exception e) {
+                throw new FunctionalTestException("Problem checking acceptable response payload: ", e);
+            }
         }
         return super.calculateCustomValue(scenarioContext, key);
     }
