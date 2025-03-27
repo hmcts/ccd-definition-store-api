5eaff0e968e7cdc119945047f253519eea56ba1e
JamesCollettCGI <james.collett@hmcts.net>
2025-03-27 14:00:38 +0000

    Remove unused import

--- excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/azurestorage/service/AzureBlobStorageClient.java
+++ excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/azurestorage/service/AzureBlobStorageClient.java
@@ -4,7 +4,6 @@ import com.microsoft.azure.storage.StorageException;
 import com.microsoft.azure.storage.blob.CloudBlobContainer;
 import com.microsoft.azure.storage.blob.CloudBlockBlob;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
