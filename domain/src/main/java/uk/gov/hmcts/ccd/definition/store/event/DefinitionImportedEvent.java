464e5264830e389859f213209de1d3c4f6325552
Sammi Chong <133241942+SammiChong@users.noreply.github.com>
2025-06-11 14:42:37 +0100

    CME-238: Elasticsearch Reindexing (#1543)
    * CME-238: Elasticsearch Indexing
    
    * Update ElasticDefinitionImportListener.java
    
    * Change default
    
    * Update ElasticDefinitionImportListener.java
    
    * Use RegEx to incrementIndexNumber
    
    * Use reindexAsync()
    
    * Add tests
    
    * Update ElasticDefinitionImportListenerTest.java
    
    * Update ElasticDefinitionImportListenerTest.java
    
    * Fix IT
    
    * Update ElasticDefinitionImportListener.java
    
    * Update ElasticDefinitionImportListener.java
    
    ---------
    
    Co-authored-by: amina-khatunCGI <amina.khatun@hmcts.net>

--- domain/src/main/java/uk/gov/hmcts/ccd/definition/store/event/DefinitionImportedEvent.java
+++ domain/src/main/java/uk/gov/hmcts/ccd/definition/store/event/DefinitionImportedEvent.java
@@ -1,12 +1,27 @@
 package uk.gov.hmcts.ccd.definition.store.event;
 
+import lombok.Getter;
+import lombok.Setter;
 import uk.gov.hmcts.ccd.definition.store.repository.entity.CaseTypeEntity;
 
 import java.util.List;
 
+@Getter
 public class DefinitionImportedEvent extends ImportEvent<List<CaseTypeEntity>> {
+
+    private final boolean reindex;
+    private final boolean deleteOldIndex;
+    @Setter private String taskId;
+
+    public DefinitionImportedEvent(List<CaseTypeEntity> caseTypes, boolean reindex, boolean deleteOldIndex) {
+        super(caseTypes);
+        this.reindex = reindex;
+        this.deleteOldIndex = deleteOldIndex;
+    }
 
     public DefinitionImportedEvent(List<CaseTypeEntity> caseTypes) {
         super(caseTypes);
+        this.reindex = false;
+        this.deleteOldIndex = false;
     }
 }
