536c9a849f8293e2399eafd355cbce6b61ff08ec
Dinesh Patel <dinesh1.patel@btinternet.com>
2025-06-27 17:19:43 +0100

    updated to add media type for file picker to show in swagger ui

--- excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/endpoint/ImportController.java
+++ excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/endpoint/ImportController.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,7 +36,7 @@ public class ImportController {
         this.processUploadServiceImpl = processUploadServiceImpl;
     }
 
-    @RequestMapping(value = URI_IMPORT, method = RequestMethod.POST)
+    @RequestMapping(value = URI_IMPORT, method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 
     @ApiResponses(value = {
         @ApiResponse(code = 201, message = ProcessUploadService.SUCCESSFULLY_CREATED),
