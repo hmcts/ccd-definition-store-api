c1780b795dc64e13a086bc1d59eb716bf9b5af5b
Dinesh Patel <dinesh1.patel@btinternet.com>
2025-07-01 17:00:05 +0100

    fix for multipart file field in swagger

--- excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/endpoint/ImportController.java
+++ excel-importer/src/main/java/uk/gov/hmcts/ccd/definition/store/excel/endpoint/ImportController.java
@@ -1,17 +1,13 @@
 package uk.gov.hmcts.ccd.definition.store.excel.endpoint;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import uk.gov.hmcts.ccd.definition.store.excel.service.ProcessUploadService;
 import uk.gov.hmcts.ccd.definition.store.excel.service.ProcessUploadServiceImpl;
@@ -58,17 +54,9 @@ public class ImportController {
         @ApiResponse(code = 500, message = "Internal Server Error")
     })
 
-    @ApiImplicitParams({
-        @ApiImplicitParam(
-                name = "file",
-                value = "File to upload",
-                required = true,
-                dataType = "file",
-                paramType = "form"
-                ),
-    })
     public ResponseEntity<String> processUpload(
-        @RequestParam("file") MultipartFile file,
+        @ApiParam(value="File to be uploaded", required = true)
+        @RequestPart("file") MultipartFile file,
         @RequestParam(value = "reindex", required = false, defaultValue = "false") Boolean reindex,
         @RequestParam(value = "deleteOldIndex", required = false, defaultValue = "false") Boolean deleteOldIndex
     ) throws IOException {
