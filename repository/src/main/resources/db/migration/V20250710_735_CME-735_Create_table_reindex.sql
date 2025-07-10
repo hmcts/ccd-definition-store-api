770155d59dcb4b25b305041d191af9ec1e5b6324
Sammi Chong <shinyee.chong@hmcts.net>
2025-07-10 18:29:46 +0100

    Update V20250710_735_CME-735_Create_table_reindex.sql

--- repository/src/main/resources/db/migration/V20250710_735_CME-735_Create_table_reindex.sql
+++ repository/src/main/resources/db/migration/V20250710_735_CME-735_Create_table_reindex.sql
@@ -1,16 +1,5 @@
---
--- Name: reindex; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.reindex (
-                                        id integer NOT NULL,
-                                        start_time timestamp NOT NULL,
-                                        end_time timestamp NOT NULL,
-                                        status character varying(50) NOT NULL,
-                                        case_type_id integer NOT NULL,
-                                        jurisdiction character varying(70) NOT NULL,
-                                        message character varying(200)
-);
+DROP TABLE IF EXISTS public.reindex CASCADE;
+DROP SEQUENCE IF EXISTS public.reindex_id_seq;
 --
 -- Name: reindex_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
@@ -23,28 +12,23 @@ CREATE SEQUENCE public.reindex_id_seq
     CACHE 1;
 
 --
+-- Name: reindex; Type: TABLE; Schema: public; Owner: -
+--
+CREATE TABLE public.reindex (
+    id             integer NOT NULL DEFAULT nextval('public.reindex_id_seq'::regclass),
+    start_time     timestamp NOT NULL,
+    end_time       timestamp NOT NULL,
+    status         varchar(50) NOT NULL,
+    case_type_id   integer NOT NULL,
+    jurisdiction   varchar(70) NOT NULL,
+    message        varchar(200),
+    CONSTRAINT pk_reindex PRIMARY KEY (id),
+    CONSTRAINT fk_reindex_case_type_id FOREIGN KEY (case_type_id) REFERENCES public.case_type(id)
+);
+
+--
 -- Name: reindex_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.reindex_id_seq OWNED BY public.reindex.id;
-
---
--- Name: reindex fk_reindex_id; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.reindex
-    ADD CONSTRAINT fk_reindex_case_type_id FOREIGN KEY (case_type_id) REFERENCES public.case_type(id);
-
---
--- Name: reindex pk_reindex; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.reindex
-    ADD CONSTRAINT pk_reindex PRIMARY KEY (id);
-
---
--- Name: reindex id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.reindex ALTER COLUMN id SET DEFAULT nextval('public.reindex_id_seq'::regclass);
 
