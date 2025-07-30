245d9d26dc6efc709248e50faa6524ec97ca8e41
dinesh1patel <74076102+dinesh1patel@users.noreply.github.com>
2025-07-30 11:49:13 +0100

    Update SwaggerConfiguration.java

--- application/src/main/java/uk/gov/hmcts/ccd/definition/store/SwaggerConfiguration.java
+++ application/src/main/java/uk/gov/hmcts/ccd/definition/store/SwaggerConfiguration.java
@@ -2,6 +2,9 @@ package uk.gov.hmcts.ccd.definition.store;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
