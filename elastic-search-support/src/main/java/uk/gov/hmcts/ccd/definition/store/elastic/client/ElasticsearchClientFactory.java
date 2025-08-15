35c0a6126bd013eaaf549f2f820dafee9478f443
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2025-08-15 22:16:52 +0100

    CCD-5849: add logger in case putSettings fails

--- elastic-search-support/src/main/java/uk/gov/hmcts/ccd/definition/store/elastic/client/ElasticsearchClientFactory.java
+++ elastic-search-support/src/main/java/uk/gov/hmcts/ccd/definition/store/elastic/client/ElasticsearchClientFactory.java
@@ -5,6 +5,8 @@ import co.elastic.clients.json.JsonData;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.elasticsearch.client.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Map;
@@ -12,6 +14,7 @@ import java.util.function.Supplier;
 
 public class ElasticsearchClientFactory {
 
+    private static final Logger log = LoggerFactory.getLogger(ElasticsearchClientFactory.class);
     private final Supplier<RestClient> restClientSupplier;
     private final JacksonJsonpMapper mapper;
 
@@ -29,7 +32,7 @@ public class ElasticsearchClientFactory {
                 s.persistent(Map.of("action.destructive_requires_name", JsonData.of(false)))
             );
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error(e.getMessage(), e);
         }
 
         return client;
