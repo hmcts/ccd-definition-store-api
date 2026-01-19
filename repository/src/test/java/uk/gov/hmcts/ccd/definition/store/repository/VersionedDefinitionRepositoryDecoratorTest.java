74c5822029d9135c2987e3a4a795d6e73b9d7aca
Dinesh Patel <dinesh1.patel@btinternet.com>
2026-01-19 17:07:10 +0000

    build fix

--- repository/src/test/java/uk/gov/hmcts/ccd/definition/store/repository/VersionedDefinitionRepositoryDecoratorTest.java
+++ repository/src/test/java/uk/gov/hmcts/ccd/definition/store/repository/VersionedDefinitionRepositoryDecoratorTest.java
@@ -37,6 +37,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
