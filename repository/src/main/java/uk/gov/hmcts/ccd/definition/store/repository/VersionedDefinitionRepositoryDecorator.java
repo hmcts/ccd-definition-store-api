60643962f35507040d6e5e05540b65eb5d1a0d2d
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2026-02-18 23:07:42 +0000

    Revert "CCD-6594: Definition import failures: ERROR: duplicate key value violates unique constraint"
    This reverts commit 452597d7d5f93bd08a8f5646812f781a5e7d1df0.

--- repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/VersionedDefinitionRepositoryDecorator.java
+++ repository/src/main/java/uk/gov/hmcts/ccd/definition/store/repository/VersionedDefinitionRepositoryDecorator.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.ccd.definition.store.repository;
 
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Example;
 import org.springframework.data.repository.query.FluentQuery;
 import uk.gov.hmcts.ccd.definition.store.repository.entity.Versionable;
@@ -9,8 +8,6 @@ import java.io.Serializable;
 import java.util.Optional;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.function.Function;
 
 @SuppressWarnings({"checkstyle:InterfaceTypeParameterName", "checkstyle:ClassTypeParameterName"})
@@ -23,59 +20,18 @@ public class VersionedDefinitionRepositoryDecorator<T extends Versionable, ID ex
 
     @Override
     public <S extends T> S save(S s) {
-        try {
-            assignVersion(s);
-            return repository.save(s);
-        } catch (DataIntegrityViolationException e) {
-            // Same file uploaded from another invocation may have committed a version we did not see
-            assignVersion(s);
-            return repository.save(s);
-        }
-    }
-
-    private <S extends T> void assignVersion(S s) {
         final Optional<Integer> version = repository.findLastVersion(s.getReference());
         s.setVersion(1 + version.orElse(0));
+        return repository.save(s);
     }
 
     @Override
     public <S extends T> List<S> saveAll(Iterable<S> iterable) {
-        final List<S> list = toList(iterable);
-        assignVersionsForBatch(list);
-        try {
-            return repository.saveAll(list);
-        } catch (DataIntegrityViolationException e) {
-            // Same file uploaded from another invocation (e.g. concurrent or sequential) may have
-            // committed versions we did not see; re-assign versions from current DB and retry once
-            assignVersionsForBatch(list);
-            return repository.saveAll(list);
+        for (S s : iterable) {
+            final Optional<Integer> version = repository.findLastVersion(s.getReference());
+            s.setVersion(1 + version.orElse(0));
         }
-    }
-
-    private <S extends T> List<S> toList(Iterable<S> iterable) {
-        if (iterable instanceof List) {
-            return (List<S>) iterable;
-        }
-        List<S> list = new ArrayList<>();
-        iterable.forEach(list::add);
-        return list;
-    }
-
-    /**
-     * Assigns versions so that (1) each reference gets the next version after the last in DB,
-     * and (2) within the same batch, duplicate references get incrementing versions to avoid
-     * unique constraint violations when saveAll is called.
-     */
-    private <S extends T> void assignVersionsForBatch(List<S> list) {
-        final Map<String, Integer> nextVersionPerReference = new HashMap<>();
-        for (S s : list) {
-            final String reference = s.getReference();
-            final int lastFromDb = repository.findLastVersion(reference).orElse(0);
-            final int lastInBatch = nextVersionPerReference.getOrDefault(reference, lastFromDb);
-            final int nextVersion = lastInBatch + 1;
-            nextVersionPerReference.put(reference, nextVersion);
-            s.setVersion(nextVersion);
-        }
+        return repository.saveAll(iterable);
     }
 
     @Override
