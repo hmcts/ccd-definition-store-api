6a77debee911a39138d89ca19c47e3d1ca59b265
kaanaktas <aktaskaan@gmail.com>
2026-07-01 14:55:08 +0100

    Remove unused `AbstractHttpConfigurer` import from SecurityConfiguration

--- application/src/main/java/uk/gov/hmcts/ccd/definition/store/SecurityConfiguration.java
+++ application/src/main/java/uk/gov/hmcts/ccd/definition/store/SecurityConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.Jwt;
