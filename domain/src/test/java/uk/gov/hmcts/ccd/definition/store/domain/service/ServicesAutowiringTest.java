a201f82b585b9f219efd0bcad9e74f8fc1b56987
patelila <ila.patel@hmcts.net>
2026-07-14 11:52:08 +0100

    Refactor ServicesAutowiringTest to explicit validator wiring

--- domain/src/test/java/uk/gov/hmcts/ccd/definition/store/domain/service/ServicesAutowiringTest.java
+++ domain/src/test/java/uk/gov/hmcts/ccd/definition/store/domain/service/ServicesAutowiringTest.java
@@ -2,23 +2,25 @@ package uk.gov.hmcts.ccd.definition.store.domain.service;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
-import jakarta.persistence.EntityManagerFactory;
-import uk.gov.hmcts.ccd.definition.store.AppInsights;
+import uk.gov.hmcts.ccd.definition.store.domain.service.casetype.CaseTypeServiceImpl;
+import uk.gov.hmcts.ccd.definition.store.domain.service.casetype.CaseTypeSnapshotService;
 import uk.gov.hmcts.ccd.definition.store.domain.service.casetype.CaseTypeService;
+import uk.gov.hmcts.ccd.definition.store.domain.service.legacyvalidation.LegacyCaseTypeValidator;
 import uk.gov.hmcts.ccd.definition.store.domain.service.metadata.MetadataFieldService;
+import uk.gov.hmcts.ccd.definition.store.domain.showcondition.ShowConditionParser;
+import uk.gov.hmcts.ccd.definition.store.domain.validation.complexfield.ComplexFieldEntityDisplayContextParameterValidatorImpl;
+import uk.gov.hmcts.ccd.definition.store.domain.validation.complexfield.ComplexFieldEntityDocumentTypeRegularExpressionValidator;
+import uk.gov.hmcts.ccd.definition.store.domain.validation.complexfield.ComplexFieldEntityShowConditionValidatorImpl;
+import uk.gov.hmcts.ccd.definition.store.domain.validation.displaycontextparameter.DisplayContextParameterValidatorFactory;
 import uk.gov.hmcts.ccd.definition.store.domain.validation.casefield.CaseFieldEntityACLValidatorImpl;
 import uk.gov.hmcts.ccd.definition.store.domain.validation.casefield.CaseFieldEntityComplexFieldsValidatorImpl;
 import uk.gov.hmcts.ccd.definition.store.domain.validation.casefield.CaseFieldEntityCrudValidatorImpl;
@@ -43,55 +45,32 @@ import uk.gov.hmcts.ccd.definition.store.domain.validation.eventcasefield.EventC
 import uk.gov.hmcts.ccd.definition.store.domain.validation.eventcasefield.EventCaseFieldShowConditionValidatorImpl;
 import uk.gov.hmcts.ccd.definition.store.domain.validation.fieldtype.BaseReferenceFieldTypeValidator;
 import uk.gov.hmcts.ccd.definition.store.domain.validation.fieldtype.FieldTypeComplexFieldsValidatorImpl;
+import uk.gov.hmcts.ccd.definition.store.domain.validation.fieldtype.FieldTypeValidationContextFactory;
 import uk.gov.hmcts.ccd.definition.store.domain.validation.fieldtype.FieldTypeValidator;
 import uk.gov.hmcts.ccd.definition.store.domain.validation.searchaliasfield.SearchAliasFieldNameValidator;
 import uk.gov.hmcts.ccd.definition.store.domain.validation.searchaliasfield.SearchAliasFieldTypeValidator;
 import uk.gov.hmcts.ccd.definition.store.domain.validation.searchaliasfield.SearchAliasFieldUnicityValidator;
-import uk.gov.hmcts.ccd.definition.store.repository.AccessProfileRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.AccessTypeRolesRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.ImportJobRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.AccessTypesRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.BannerRepository;
 import uk.gov.hmcts.ccd.definition.store.repository.CaseFieldEntityUtil;
-import uk.gov.hmcts.ccd.definition.store.repository.CaseFieldRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.CaseRoleRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.CaseTypeLiteRepository;
 import uk.gov.hmcts.ccd.definition.store.repository.CaseTypeRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.CaseTypeSnapshotRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.CategoryTabRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.ChallengeQuestionTabRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.DisplayGroupRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.DraftDefinitionRepositoryDecorator;
-import uk.gov.hmcts.ccd.definition.store.repository.EventRepository;
 import uk.gov.hmcts.ccd.definition.store.repository.FieldTypeRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.GenericLayoutRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.JurisdictionRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.JurisdictionUiConfigRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.RoleToAccessProfilesRepository;
 import uk.gov.hmcts.ccd.definition.store.repository.SearchAliasFieldRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.SearchCriteriaRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.SearchPartyRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.SecurityUtils;
-import uk.gov.hmcts.ccd.definition.store.repository.SnapshotJdbcRepository;
-import uk.gov.hmcts.ccd.definition.store.repository.entity.FieldTypeEntity;
-import uk.gov.hmcts.ccd.definition.store.repository.model.DefinitionModelMapper;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
-class ServicesAutowiringTest implements ApplicationContextAware {
+class ServicesAutowiringTest {
 
-    private ApplicationContext applicationContext;
+    @Autowired
+    private CaseTypeService caseTypeService;
+
+    @Autowired
+    private FieldTypeService fieldTypeService;
 
     @Test
     void caseTypeServiceHasAllRequiredValidatorsWiredIntoList() {
-
-        CaseTypeService caseTypeService
-            = applicationContext.getAutowireCapableBeanFactory().getBean(CaseTypeService.class);
 
         List<CaseTypeEntityValidator> caseTypeEntityValidators
             = (List<CaseTypeEntityValidator>) ReflectionTestUtils.getField(caseTypeService, "caseTypeEntityValidators");
@@ -187,9 +166,6 @@ class ServicesAutowiringTest implements ApplicationContextAware {
     @Test
     void fieldTypeServiceHasAllRequiredValidatorsWiredIntoList() {
 
-        FieldTypeService fieldTypeService
-            = applicationContext.getAutowireCapableBeanFactory().getBean(FieldTypeService.class);
-
         List<FieldTypeValidator> fieldTypeValidators
             = (List<FieldTypeValidator>) ReflectionTestUtils.getField(fieldTypeService, "validators");
 
@@ -199,11 +175,16 @@ class ServicesAutowiringTest implements ApplicationContextAware {
             FieldTypeComplexFieldsValidatorImpl.class
         );
 
-    }
+        assertListOfComponentsContainInstances(
+            (List) ReflectionTestUtils.getField(
+                getValidator(fieldTypeValidators, FieldTypeComplexFieldsValidatorImpl.class),
+                "complexFieldValidators"
+            ),
+            ComplexFieldEntityDisplayContextParameterValidatorImpl.class,
+            ComplexFieldEntityDocumentTypeRegularExpressionValidator.class,
+            ComplexFieldEntityShowConditionValidatorImpl.class
+        );
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
     }
 
     private <T> T getValidator(List objects, Class<T> validatorClass) {
@@ -232,183 +213,110 @@ class ServicesAutowiringTest implements ApplicationContextAware {
     }
 
     @Configuration
-    @ComponentScan(basePackages = {"uk.gov.hmcts.ccd.definition.store.domain"}
-    )
+    @Import({
+        CaseTypeServiceImpl.class,
+        FieldTypeServiceImpl.class,
+        CaseTypeEntitySecurityClassificationValidatorImpl.class,
+        CaseTypeEntityCaseFieldsValidatorImpl.class,
+        CaseTypeEntityEventValidatorImpl.class,
+        CaseTypeEntityACLValidatorImpl.class,
+        CaseTypeEntityCrudValidatorImpl.class,
+        CaseTypeEntitySearchAliasFieldsValidatorImpl.class,
+        CaseFieldEntitySecurityClassificationValidatorImpl.class,
+        CaseFieldEntityComplexFieldsValidatorImpl.class,
+        CaseFieldEntityACLValidatorImpl.class,
+        CaseFieldEntityCrudValidatorImpl.class,
+        ComplexFieldEntitySecurityClassificationValidatorImpl.class,
+        EventEntityEventCaseFieldsValidatorImpl.class,
+        EventEntitySecurityClassificationValidatorImpl.class,
+        EventEntityCrudValidatorImpl.class,
+        EventEntityACLValidatorImpl.class,
+        EventEntityCreateEventValidator.class,
+        EventEntityPostStateValidator.class,
+        EventEntityEnablingConditionValidator.class,
+        EventCaseFieldLabelCaseFieldValidator.class,
+        EventCaseFieldShowConditionValidatorImpl.class,
+        EventCaseFieldDisplayContextValidatorImpl.class,
+        BaseReferenceFieldTypeValidator.class,
+        FieldTypeComplexFieldsValidatorImpl.class,
+        ComplexFieldEntityDisplayContextParameterValidatorImpl.class,
+        ComplexFieldEntityDocumentTypeRegularExpressionValidator.class,
+        ComplexFieldEntityShowConditionValidatorImpl.class
+    })
     public static class Config {
 
         @Bean
-        @Primary
-        public AppInsights appInsights() {
-            return mock(AppInsights.class);
-        }
-
-        @Bean
-        @Primary
         public CaseTypeRepository caseTypeRepository() {
             return mock(CaseTypeRepository.class);
         }
 
         @Bean
-        @Primary
-        public ChallengeQuestionTabRepository challengeQuestionTabRepository() {
-            return mock(ChallengeQuestionTabRepository.class);
+        public EntityToResponseDTOMapper entityToResponseDTOMapper() {
+            return mock(EntityToResponseDTOMapper.class);
         }
 
         @Bean
-        @Primary
-        public CaseTypeLiteRepository caseTypeLiteRepository() {
-            return mock(CaseTypeLiteRepository.class);
+        public LegacyCaseTypeValidator legacyCaseTypeValidator() {
+            return mock(LegacyCaseTypeValidator.class);
         }
 
         @Bean
-        @Primary
-        public CategoryTabRepository categoryTabRepository() {
-            return mock(CategoryTabRepository.class);
-        }
-
-
-        @Bean
-        @Primary
-        public BannerRepository bannerRepository() {
-            return mock(BannerRepository.class);
-        }
-
-        @Bean
-        @Primary
-        public JurisdictionUiConfigRepository jurisdictionUiConfigRepository() {
-            return mock(JurisdictionUiConfigRepository.class);
-        }
-
-        @Bean
-        @Primary
-        public FieldTypeRepository fieldTypeRepository() {
-            FieldTypeRepository fieldTypeRepository = mock(FieldTypeRepository.class);
-            FieldTypeEntity textBaseType = new FieldTypeEntity();
-            textBaseType.setReference("Text");
-            when(fieldTypeRepository.findBaseType(eq("Text"))).thenReturn(
-                Optional.of(textBaseType)
-            );
-            return fieldTypeRepository;
-        }
-
-        @Bean
-        @Primary
-        public JurisdictionRepository jurisdictionRepository() {
-            return mock(JurisdictionRepository.class);
-        }
-
-        @Bean
-        @Primary
-        public GenericLayoutRepository genericLayoutRepository() {
-            return mock(GenericLayoutRepository.class);
-        }
-
-        @Bean
-        @Primary
-        public DisplayGroupRepository displayGroupRepository() {
-            return mock(DisplayGroupRepository.class);
-        }
-
-        @Bean
-        @Primary
-        public AccessProfileRepository accessProfileRepository() {
-            return mock(AccessProfileRepository.class);
-        }
-
-        @Bean
-        @Primary
-        public AccessTypeRolesRepository accessTypeRolesRepository() {
-            return mock(AccessTypeRolesRepository.class);
-        }
-
-        @Bean
-        @Primary
-        public AccessTypesRepository accessTypeRepository() {
-            return mock(AccessTypesRepository.class);
-        }
-
-        @Bean
-        @Primary
-        public EventRepository eventRepository() {
-            return mock(EventRepository.class);
-        }
-
-        @Bean
-        @Primary
-        public SecurityUtils securityUtils() {
-            return mock(SecurityUtils.class);
-        }
-
-        @Bean
-        @Primary
-        public CaseFieldRepository caseFieldRepository() {
-            return mock(CaseFieldRepository.class);
-        }
-
-        @Bean
-        @Primary
         public MetadataFieldService metadataFieldService() {
             return mock(MetadataFieldService.class);
         }
 
         @Bean
-        @Primary
-        public CaseRoleRepository caseRoleRepository() {
-            return mock(CaseRoleRepository.class);
+        public CaseTypeSnapshotService caseTypeSnapshotService() {
+            return mock(CaseTypeSnapshotService.class);
         }
 
         @Bean
-        @Primary
-        public DraftDefinitionRepositoryDecorator draftDefinitionRepositoryDecorator() {
-            return mock(DraftDefinitionRepositoryDecorator.class);
+        public FieldTypeRepository fieldTypeRepository() {
+            FieldTypeRepository fieldTypeRepository = mock(FieldTypeRepository.class);
+            when(fieldTypeRepository.findPredefinedComplexTypes()).thenReturn(List.of());
+            return fieldTypeRepository;
         }
 
         @Bean
-        @Primary
         public CaseFieldEntityUtil caseFieldEntityUtil() {
             return mock(CaseFieldEntityUtil.class);
         }
 
         @Bean
-        @Primary
-        public DefinitionModelMapper definitionModelMapper() {
-            return mock(DefinitionModelMapper.class);
+        public ShowConditionParser showConditionParser() {
+            return mock(ShowConditionParser.class);
         }
 
         @Bean
-        @Primary
         public SearchAliasFieldRepository searchAliasFieldRepository() {
             return mock(SearchAliasFieldRepository.class);
         }
 
         @Bean
-        @Primary
-        public RoleToAccessProfilesRepository roleToAccessProfileRepository() {
-            return mock(RoleToAccessProfilesRepository.class);
+        public SearchAliasFieldTypeValidator searchAliasFieldTypeValidator(
+            SearchAliasFieldRepository searchAliasFieldRepository
+        ) {
+            return new SearchAliasFieldTypeValidator(searchAliasFieldRepository);
         }
 
         @Bean
-        @Primary
-        public SearchCriteriaRepository getSearchCriteriaRepository() {
-            return mock(SearchCriteriaRepository.class);
+        public SearchAliasFieldUnicityValidator searchAliasFieldUnicityValidator() {
+            return new SearchAliasFieldUnicityValidator();
         }
 
         @Bean
-        @Primary
-        public SearchPartyRepository searchPartyRepository() {
-            return mock(SearchPartyRepository.class);
+        public SearchAliasFieldNameValidator searchAliasFieldNameValidator() {
+            return new SearchAliasFieldNameValidator();
         }
 
         @Bean
-        @Primary
-        public CaseTypeSnapshotRepository caseTypeSnapshotRepository() {
-            return mock(CaseTypeSnapshotRepository.class);
+        public FieldTypeValidationContextFactory fieldTypeValidationContextFactory() {
+            return mock(FieldTypeValidationContextFactory.class);
         }
 
         @Bean
-        @Primary
-        public SnapshotJdbcRepository snapshotJdbcRepository() {
-            return mock(SnapshotJdbcRepository.class);
+        public DisplayContextParameterValidatorFactory displayContextParameterValidatorFactory() {
+            return mock(DisplayContextParameterValidatorFactory.class);
         }
     }
 }
